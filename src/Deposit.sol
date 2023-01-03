4b3ccdb02156667ccca816e41dedc03a5427e05b
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-03 15:42:52 +0200

    Added minting of BNFT

--- src/Deposit.sol
+++ src/Deposit.sol
@@ -2,17 +2,21 @@
 pragma solidity 0.8.13;
 
 import "./interfaces/ITNFT.sol";
+import "./interfaces/IBNFT.sol";
 
 contract Deposit {
 
     ITNFT TNFTInstance;
+    IBNFT BNFTInstance;
 
-    constructor(address _TNFTAddress) {
+    constructor(address _TNFTAddress, address _BNFTAddress) {
         TNFTInstance = ITNFT(_TNFTAddress);
+        BNFTInstance = IBNFT(_BNFTAddress);
     }
 
     function deposit() public payable {
         require(msg.value >= 32 ether, "Insufficient staking amount");
         TNFTInstance.mint(msg.sender);
+        BNFTInstance.mint(msg.sender);
     }
 }
