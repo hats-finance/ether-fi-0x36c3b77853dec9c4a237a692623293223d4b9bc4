b66b35d5a27367c6dc0009d54aa8b331dc1a6554
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-03 18:30:15 +0200

    Initial

--- src/BNFT.sol
+++ src/BNFT.sol
@@ -5,20 +5,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BNFT is ERC721, Ownable {
-
     uint256 private tokenId;
     uint256 public price = 2 ether;
 
-    constructor() ERC721("Bond NFT", "BNFT"){
-
-    }
+    constructor() ERC721("Bond NFT", "BNFT") {}
 
     function mint(address _reciever) external onlyOwner {
         _safeMint(_reciever, tokenId);
         unchecked {
-           tokenId++;
+            tokenId++;
         }
     }
-
-    //Make soulbound
 }
