24e8a7e19cf82e8532dff1b4565ef76801262736
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-03 14:10:02 +0200

    Installed OZ contracts and created TNFT constructor

--- src/TNFT.sol
+++ src/TNFT.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.8.15;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TNFT is ERC721 {
+
+    constructor() ERC721("Transferrable NFT", "TNFT"){
+        
+    }
     
 }
