94b19ef49f3643191f5eeb1d0eafc37a945f1324
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-03 16:04:03 +0200

    Added setup in test

--- test/Deposit.t.sol
+++ test/Deposit.t.sol
@@ -2,7 +2,19 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/Deposit.sol";
 
 contract DepositTest is Test {
+
+    Deposit public depositInstance;
+
+    address owner = vm.addr(1);
+    address staker = vm.addr(2);
+
+    function setUp() public {
+        vm.startPrank(owner);
+        depositInstance = new Deposit();
+        vm.stopPrank();
+    }
 
 }
