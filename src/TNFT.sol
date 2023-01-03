a4dc08d072e078ad58f3e49993101bbb076834b5
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-03 14:16:44 +0200

    Added mint function for TNFT

--- src/TNFT.sol
+++ src/TNFT.sol
@@ -5,8 +5,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TNFT is ERC721 {
 
+    uint256 private tokenId;
+
     constructor() ERC721("Transferrable NFT", "TNFT"){
-        
+
+    }
+
+    function mint(address _reciever) public {
+        _mint(_reciever, tokenId);
+        tokenId++;
     }
     
 }
