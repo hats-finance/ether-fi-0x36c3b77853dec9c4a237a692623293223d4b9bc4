45a78f9c2ee3b4bda205ac5c327a854a883f924f
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-04 14:03:45 +0200

    Cleaned up interfaces

--- src/interfaces/IBNFT.sol
+++ src/interfaces/IBNFT.sol
@@ -4,6 +4,5 @@ pragma solidity 0.8.13;
 interface IBNFT {
 
     function mint(address _reciever) external;
-    function setNftValue(uint256 _newNftValue) external;
 
 }
--- src/interfaces/ITNFT.sol
+++ src/interfaces/ITNFT.sol
@@ -4,5 +4,4 @@ pragma solidity 0.8.13;
 interface ITNFT {
 
     function mint(address _reciever) external;
-    function setNftValue(uint256 _newNftValue) external;
 }
