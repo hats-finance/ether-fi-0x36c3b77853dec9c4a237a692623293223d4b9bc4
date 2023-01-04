5efe480969d3be1225535a99faf8a08f64d5a8a4
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-04 15:14:21 +0200

    Created start auction function and data structures

---
+++ src/Auction.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "./interfaces/ITNFT.sol";
+import "./interfaces/IBNFT.sol";
+import "./interfaces/IDeposit.sol";
+import "./TNFT.sol";
+import "./BNFT.sol";
+import "./Deposit.sol";
+
+contract Auction {
+
+    uint256 public numberofAuctions;
+
+    mapping(uint256 => Auction) public auctions;
+
+    struct Auction {
+        uint256 reservePrice;
+        uint256 winningBid;
+        uint256 startTime;
+        uint256 timeClosed;
+        address winningAddress;
+        bool isActive;
+    }
+
+    //only called by deposit contract or owner
+    function startAuction() external {
+
+        auctions[numberofAuctions] = Auction({
+            reservePrice: 0,
+            winningBid: 0,
+            startTime: block.timestamp,
+            timeClosed: 0,
+            winningAddress: address(0),
+            isActive: true
+        });
+
+        numberofAuctions++;
+
+    }
+
+}
