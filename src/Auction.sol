f11a30e38c7aaafda42334fd99eaef2afd7f37bf
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-05 18:14:56 +0200

    Updated auction functions

--- src/Auction.sol
+++ src/Auction.sol
@@ -10,7 +10,6 @@ import "./BNFT.sol";
 import "./Deposit.sol";
 
 contract Auction is IAuction {
-    //uint256 public numberOfAuctions;
     address public depositContractAddress;
     address public owner;
 
@@ -19,55 +18,29 @@ contract Auction is IAuction {
 
     bool public bidsEnabled;
 
-    mapping(uint256 => AuctionDetails) public auctions;
     mapping(address => mapping(uint256 => Bid)) public bids;
-    //mapping(address => uint256) public refundBalances;
 
-    //event AuctionCreated(uint256 auctionId, uint256 startTime);
-    //event AuctionClosed(uint256 auctionId, uint256 endTime);
     event BidPlaced(address bidder, uint256 amount, uint256 bidderId);
-    //event RefundClaimed(address claimer, uint256 amount);
 
     constructor() {
         owner = msg.sender;
     }
 
-    // function startAuction() public onlyOwnerOrDepositContract {
-    //     if (numberOfAuctions != 0) {
-    //         require(
-    //             auctions[numberOfAuctions - 1].isActive == false,
-    //             "Previous auction not closed"
-    //         );
-    //     }
-
-    //     auctions[numberOfAuctions] = AuctionDetails({
-    //         winningBidId: 0,
-    //         numberOfBids: 0,
-    //         startTime: block.timestamp,
-    //         timeClosed: 0,
-    //         isActive: true
-    //     });
-
-    //     emit AuctionCreated(numberOfAuctions, block.timestamp);
-    //     numberOfAuctions++;
-    // }
-
-    //Owner cannot call this otherwise it poses a bias risk between bidder and owner
-    // function closeAuction() external onlyDepositContract returns (address) {
-    //     AuctionDetails storage auctionDetails = auctions[numberOfAuctions - 1];
-    //     auctionDetails.isActive = false;
-    //     auctionDetails.timeClosed = block.timestamp;
-
-    //     emit AuctionClosed(numberOfAuctions - 1, block.timestamp);
-
-    //     uint256 winningBidID = auctionDetails.winningBidId;
-    //     Bid memory bid = bids[numberOfAuctions - 1][winningBidID];
-    //     return bid.bidderAddress;
-    // }
-
-    function disableBidding() external onlyDepositContract {
+    function disableBidding() external onlyDepositContract returns (address){
         require(bidsEnabled == true, "Bids already disabled");
         bidsEnabled = false;
+        bids[currentHighestBidId].isActive = false;
+        
+        uint256 tempWinningBidId;
+
+        for(uint256 x = 1; x <= numberOfBids; x++){
+            if((bids[x].amount > bids[tempWinningBidId].amount) && (bids[x].isActive == true)){
+                tempWinningBidId = x;
+            }
+        }
+
+        currentHighestBidId = tempWinningBidId;
+        return bids[currentHighestBidId].bidderAddress;
     }
 
     function enableBidding() external onlyDepositContract {
@@ -104,7 +77,6 @@ contract Auction is IAuction {
     function bidOnStake() external payable {
 
         require(bidsEnabled == true, "Bidding is on hold");
-        //require(msg.value > bid.amount, "Bid too low");
 
         bids[numberOfBids] = Bid({
             amount: msg.value,
@@ -116,27 +88,10 @@ contract Auction is IAuction {
             currentHighestBidId = numberOfBids;
         }
 
-        //currentAuction.numberOfBids++;
-        //refundBalances[bid.bidderAddress] += bid.amount;
-
-        //currentAuction.winningBidId = currentAuction.numberOfBids - 1;
-
         numberOfBids++;
 
         emit BidPlaced(msg.sender, msg.value, numberOfBids - 1);
     }
-
-    // function claimRefundableBalance() external {
-    //     require(refundBalances[msg.sender] > 0, "No refund available");
-
-    //     uint256 refundBalance = refundBalances[msg.sender];
-    //     refundBalances[msg.sender] = 0;
-
-    //     (bool sent, ) = msg.sender.call{value: refundBalance}("");
-    //     require(sent, "Failed to send Ether");
-
-    //     emit RefundClaimed(msg.sender, refundBalance);
-    // }
 
     function setDepositContractAddress(address _depositContractAddress)
         external
@@ -151,11 +106,6 @@ contract Auction is IAuction {
         );
         _;
     }
-
-    // modifier onlyOwner() {
-    //     require(msg.sender == owner, "Only owner function");
-    //     _;
-    // }
 
     modifier onlyDepositContract() {
         require(
