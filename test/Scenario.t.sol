d9d66515dd432776ae136f2c96672128051678be
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-05 21:54:54 +0200

    Updated Scenario tests

--- test/Scenario.t.sol
+++ test/Scenario.t.sol
@@ -1,195 +1,115 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import "forge-std/Test.sol";
-// import "../src/Deposit.sol";
-// import "../src/BNFT.sol";
-// import "../src/TNFT.sol";
-// import "../src/Auction.sol";
+import "forge-std/Test.sol";
+import "../src/Deposit.sol";
+import "../src/BNFT.sol";
+import "../src/TNFT.sol";
+import "../src/Auction.sol";
 
-// contract ScenarioTest is Test {
-//     Deposit public depositInstance;
-//     BNFT public TestBNFTInstance;
-//     TNFT public TestTNFTInstance;
-//     Auction public auctionInstance;
+contract ScenarioTest is Test {
+    Deposit public depositInstance;
+    BNFT public TestBNFTInstance;
+    TNFT public TestTNFTInstance;
+    Auction public auctionInstance;
 
-//     address owner = vm.addr(1);
-//     address alice = vm.addr(2);
+    address owner = vm.addr(1);
+    address alice = vm.addr(2);
 
-//     function setUp() public {
-//         vm.startPrank(owner);
-//         auctionInstance = new Auction();
-//         depositInstance = new Deposit(address(auctionInstance));
-//         TestBNFTInstance = BNFT(address(depositInstance.BNFTInstance()));
-//         TestTNFTInstance = TNFT(address(depositInstance.TNFTInstance()));
-//         vm.stopPrank();
-//     }
+    function setUp() public {
+        vm.startPrank(owner);
+        auctionInstance = new Auction();
+        depositInstance = new Deposit(address(auctionInstance));
+        TestBNFTInstance = BNFT(address(depositInstance.BNFTInstance()));
+        TestTNFTInstance = TNFT(address(depositInstance.TNFTInstance()));
+        vm.stopPrank();
+    }
 
-//     function testOneBidderWithOneStake() public {
-//         startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         auctionInstance.bidOnStake{value: 0.1 ether}();
+    /**
+     *  One bid - 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
+     *  One deposit - 0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf
+     */
+    function scenarioOne() public {
+        startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        auctionInstance.bidOnStake{value: 0.3 ether}();
 
-//         assertEq(
-//             auctionInstance.refundBalances(
-//                 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//             ),
-//             0
-//         );
-//         assertEq(address(auctionInstance).balance, 0.1 ether);
-//         assertEq(address(depositInstance).balance, 0);
+        assertEq(address(auctionInstance).balance, 0.3 ether);
+        assertEq(address(depositInstance).balance, 0);
+        
+        (uint256 amount,, address bidderAddress, bool isActiveBeforeStake) = auctionInstance.bids(auctionInstance.currentHighestBidId());
 
-//         (
-//             uint256 winningBidId,
-//             uint256 numberOfBids,
-//             ,
-//             ,
-//             bool isActive
-//         ) = auctionInstance.auctions(auctionInstance.numberOfAuctions() - 1);
-//         (uint256 amount, , address bidderAddress) = auctionInstance.bids(
-//             auctionInstance.numberOfAuctions() - 1,
-//             winningBidId
-//         );
+        assertEq(auctionInstance.numberOfBids() - 1, 1);
+        assertEq(amount, 0.3 ether);
+        assertEq(bidderAddress, 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        assertEq(auctionInstance.currentHighestBidId(), 1);
+        assertEq(auctionInstance.bidsEnabled(), true);
+        assertEq(isActiveBeforeStake, true);
 
-//         assertEq(winningBidId, 0);
-//         assertEq(numberOfBids, 1);
-//         assertEq(amount, 0.1 ether);
-//         assertEq(bidderAddress, 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         assertEq(isActive, true);
+        vm.stopPrank();
+        startHoax(0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf);
 
-//         depositInstance.deposit{value: 0.1 ether}();
-//         assertEq(
-//             TestBNFTInstance.ownerOf(0),
-//             0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//         );
-//         assertEq(
-//             TestTNFTInstance.ownerOf(0),
-//             0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//         );
-//         assertEq(address(depositInstance).balance, 0.1 ether);
+        depositInstance.deposit{value: 0.1 ether}();
+        assertEq(
+            TestBNFTInstance.ownerOf(0),
+            0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf
+        );
+        assertEq(
+            TestTNFTInstance.ownerOf(0),
+            0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf
+        );
+        assertEq(address(depositInstance).balance, 0.1 ether);
+        assertEq(address(auctionInstance).balance, 0.3 ether);
+        assertEq(auctionInstance.bidsEnabled(), false);
 
-//         (, , , , bool isActive2) = auctionInstance.auctions(
-//             auctionInstance.numberOfAuctions() - 1
-//         );
-//         assertEq(isActive2, false);
-//     }
+        (,,, bool isActiveAfterStake) = auctionInstance.bids(1);
+        assertEq(isActiveAfterStake, false);
+    }
 
-//     function testTwoBidderWithOneStakeAndRefundForLosingBidder() public {
-//         //Bid 1
-//         startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         auctionInstance.bidOnStake{value: 0.1 ether}();
+    /**
+     *  Three bids - 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931, 0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf, 0x2DEFD6537cF45E040639AdA147Ac3377c7C61F20
+     *  One bid cancel - 0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf
+     *  One deposit - 0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf
+     *  Attempted Bid - 0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf
+     *  Fourth Bid - 0x48809A2e8D921790C0B8b977Bbb58c5DbfC7f098
+     *  UpdatedBid - 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
+     *  Second deposit - 0x835ff0CC6F35B148b85e0E289DAeA0497ec5aA7f
+     */
+    function scenarioTwo() public {
+        
+        //Bid One
+        hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        auctionInstance.bidOnStake{0.1 ether}();
 
-//         assertEq(
-//             auctionInstance.refundBalances(
-//                 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//             ),
-//             0
-//         );
-//         assertEq(address(auctionInstance).balance, 0.1 ether);
-//         assertEq(address(depositInstance).balance, 0);
+        //Bid Two
+        hoax(0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf);
+        auctionInstance.bidOnStake{0.4 ether}();
 
-//         (
-//             uint256 winningBidIdAfterBid1,
-//             uint256 numberOfBidsAfterBid1,
-//             ,
-//             ,
-//             bool isActiveAfterBid1
-//         ) = auctionInstance.auctions(auctionInstance.numberOfAuctions() - 1);
-//         (
-//             uint256 amountOfBid1,
-//             ,
-//             address bidderAddressAfterBid1
-//         ) = auctionInstance.bids(
-//                 auctionInstance.numberOfAuctions() - 1,
-//                 winningBidIdAfterBid1
-//             );
+        //Bid Three
+        hoax(0x2DEFD6537cF45E040639AdA147Ac3377c7C61F20);
+        auctionInstance.bidOnStake{0.7 ether}();
 
-//         assertEq(winningBidIdAfterBid1, 0);
-//         assertEq(numberOfBidsAfterBid1, 1);
-//         assertEq(amountOfBid1, 0.1 ether);
-//         assertEq(
-//             bidderAddressAfterBid1,
-//             0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//         );
-//         assertEq(isActiveAfterBid1, true);
+        //Bid cancelled
+        startHoax(0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf);
+        auctionInstance.cancelBid(2);
 
-//         vm.stopPrank();
+        //Deposit One
+        depositInstance.deposit{value: 0.1 ether}();
 
-//         //Bid 2
-//         startHoax(0xCDca97f61d8EE53878cf602FF6BC2f260f10240B);
-//         auctionInstance.bidOnStake{value: 0.4 ether}();
+        //Attempted bid which should fail
+        vm.expectRevert("Bidding is on hold");
+        auctionInstance.bidOnStake{0.3 ether}();
+        vm.stopPrank();
 
-//         assertEq(
-//             auctionInstance.refundBalances(
-//                 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//             ),
-//             0.1 ether
-//         );
-//         assertEq(address(auctionInstance).balance, 0.5 ether);
-//         assertEq(address(depositInstance).balance, 0);
+        //Bid Four
+        hoax(0x48809A2e8D921790C0B8b977Bbb58c5DbfC7f098);
+        auctionInstance.bidOnStake{0.4 ether}();
 
-//         (
-//             uint256 winningBidIdAfterBid2,
-//             uint256 numberOfBidsAfterBid2,
-//             ,
-//             ,
-//             bool isActiveAfterBid2
-//         ) = auctionInstance.auctions(auctionInstance.numberOfAuctions() - 1);
-//         (
-//             uint256 amountOfBid2,
-//             ,
-//             address bidderAddressAfterBid2
-//         ) = auctionInstance.bids(
-//                 auctionInstance.numberOfAuctions() - 1,
-//                 winningBidIdAfterBid2
-//             );
+        //Bid updated
+        hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        auctionInstance.updateBid{0.9 ether}();
 
-//         assertEq(winningBidIdAfterBid2, 1);
-//         assertEq(numberOfBidsAfterBid2, 2);
-//         assertEq(isActiveAfterBid2, true);
-//         assertEq(amountOfBid2, 0.4 ether);
-//         assertEq(
-//             bidderAddressAfterBid2,
-//             0xCDca97f61d8EE53878cf602FF6BC2f260f10240B
-//         );
-
-//         depositInstance.deposit{value: 0.1 ether}();
-//         assertEq(
-//             TestBNFTInstance.ownerOf(0),
-//             0xCDca97f61d8EE53878cf602FF6BC2f260f10240B
-//         );
-//         assertEq(
-//             TestTNFTInstance.ownerOf(0),
-//             0xCDca97f61d8EE53878cf602FF6BC2f260f10240B
-//         );
-//         assertEq(address(depositInstance).balance, 0.1 ether);
-//         assertEq(
-//             auctionInstance.refundBalances(
-//                 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//             ),
-//             0.1 ether
-//         );
-
-//         (, , , , bool isActiveAfterStake) = auctionInstance.auctions(
-//             auctionInstance.numberOfAuctions() - 1
-//         );
-//         assertEq(isActiveAfterStake, false);
-
-//         vm.stopPrank();
-//         hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         auctionInstance.claimRefundableBalance();
-//         assertEq(
-//             auctionInstance.refundBalances(
-//                 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//             ),
-//             0
-//         );
-//         assertEq(
-//             auctionInstance.refundBalances(
-//                 0xCDca97f61d8EE53878cf602FF6BC2f260f10240B
-//             ),
-//             0
-//         );
-//         assertEq(address(depositInstance).balance, 0.1 ether);
-//         assertEq(address(auctionInstance).balance, 0.4 ether);
-//     }
-// }
+        //Deposit Two
+        hoax(0x835ff0CC6F35B148b85e0E289DAeA0497ec5aA7f);
+        depositInstance.deposit{value: 0.1 ether}();
+    }
+ }
