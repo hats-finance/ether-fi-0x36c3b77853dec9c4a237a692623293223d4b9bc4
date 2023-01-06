331676460218fbd4ec576b6e2dff4dea266cddb4
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-06 11:07:16 +0200

    Deposit tests updated with whitelisting

--- test/Deposit.t.sol
+++ test/Deposit.t.sol
@@ -1,107 +1,122 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import "forge-std/Test.sol";
-// import "../src/Deposit.sol";
-// import "../src/BNFT.sol";
-// import "../src/TNFT.sol";
-// import "../src/Auction.sol";
-// import "../src/Treasury.sol";
+import "forge-std/Test.sol";
+import "../src/Deposit.sol";
+import "../src/BNFT.sol";
+import "../src/TNFT.sol";
+import "../src/Auction.sol";
+import "../src/Treasury.sol";
+import "../lib/murky/src/Merkle.sol";
 
-// contract DepositTest is Test {
-//     Deposit public depositInstance;
-//     BNFT public TestBNFTInstance;
-//     TNFT public TestTNFTInstance;
-//     Auction public auctionInstance;
-//     Treasury public treasuryInstance;
+contract DepositTest is Test {
+    Deposit public depositInstance;
+    BNFT public TestBNFTInstance;
+    TNFT public TestTNFTInstance;
+    Auction public auctionInstance;
+    Treasury public treasuryInstance;
+    Merkle merkle;
+    bytes32 root;
+    bytes32[] public whiteListedAddresses;
 
-//     address owner = vm.addr(1);
-//     address alice = vm.addr(2);
+    address owner = vm.addr(1);
+    address alice = vm.addr(2);
 
-//     function setUp() public {
-//         vm.startPrank(owner);
-//         treasuryInstance = new Treasury();
-//         auctionInstance = new Auction(address(treasuryInstance));
-//         depositInstance = new Deposit(address(auctionInstance));
-//         TestBNFTInstance = BNFT(address(depositInstance.BNFTInstance()));
-//         TestTNFTInstance = TNFT(address(depositInstance.TNFTInstance()));
-//         vm.stopPrank();
-//     }
+    function setUp() public {
+        vm.startPrank(owner);
+        _merkleSetup();
+        treasuryInstance = new Treasury();
+        auctionInstance = new Auction(address(treasuryInstance), root);
+        depositInstance = new Deposit(address(auctionInstance));
+        TestBNFTInstance = BNFT(address(depositInstance.BNFTInstance()));
+        TestTNFTInstance = TNFT(address(depositInstance.TNFTInstance()));
+        vm.stopPrank();
+    }
 
-//     function testDepositContractInstantiatedCorrectly() public {
-//         assertEq(depositInstance.stakeAmount(), 0.1 ether);
-//         assertEq(depositInstance.owner(), address(owner));
-//     }
+    function testDepositContractInstantiatedCorrectly() public {
+        assertEq(depositInstance.stakeAmount(), 0.1 ether);
+        assertEq(depositInstance.owner(), address(owner));
+    }
 
-//     function testDepositCreatesNFTs() public {
-//         hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         depositInstance.deposit{value: 0.1 ether}();
-//         assertEq(
-//             TestBNFTInstance.balanceOf(
-//                 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//             ),
-//             1
-//         );
-//         assertEq(
-//             TestTNFTInstance.balanceOf(
-//                 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//             ),
-//             1
-//         );
-//     }
+    function testDepositCreatesNFTs() public {
+        hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        depositInstance.deposit{value: 0.1 ether}();
+        assertEq(
+            TestBNFTInstance.balanceOf(
+                0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
+            ),
+            1
+        );
+        assertEq(
+            TestTNFTInstance.balanceOf(
+                0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
+            ),
+            1
+        );
+    }
 
-//     function testDepositCreatesNFTsWithCorrectOwner() public {
-//         hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         depositInstance.deposit{value: 0.1 ether}();
-//         assertEq(
-//             TestBNFTInstance.ownerOf(0),
-//             0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//         );
-//         assertEq(
-//             TestTNFTInstance.ownerOf(0),
-//             0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//         );
-//     }
+    function testDepositCreatesNFTsWithCorrectOwner() public {
+        hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        depositInstance.deposit{value: 0.1 ether}();
+        assertEq(
+            TestBNFTInstance.ownerOf(0),
+            0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
+        );
+        assertEq(
+            TestTNFTInstance.ownerOf(0),
+            0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
+        );
+    }
 
-//     function testDepositReceivesEther() public {
-//         hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         depositInstance.deposit{value: 0.1 ether}();
-//         assertEq(address(depositInstance).balance, 0.1 ether);
-//     }
+    function testDepositReceivesEther() public {
+        hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        depositInstance.deposit{value: 0.1 ether}();
+        assertEq(address(depositInstance).balance, 0.1 ether);
+    }
 
-//     function testDepositUpdatesBalancesMapping() public {
-//         startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         depositInstance.deposit{value: 0.1 ether}();
-//         assertEq(
-//             depositInstance.depositorBalances(
-//                 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//             ),
-//             0.1 ether
-//         );
-//         vm.stopPrank();
-//         hoax(address(depositInstance));
-//         auctionInstance.enableBidding();
-//         vm.stopPrank();
+    function testDepositUpdatesBalancesMapping() public {
+        startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        depositInstance.deposit{value: 0.1 ether}();
+        assertEq(
+            depositInstance.depositorBalances(
+                0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
+            ),
+            0.1 ether
+        );
+        vm.stopPrank();
+        hoax(address(depositInstance));
+        auctionInstance.enableBidding();
+        vm.stopPrank();
         
-//         startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         depositInstance.deposit{value: 0.1 ether}();
-//         assertEq(
-//             depositInstance.depositorBalances(
-//                 0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
-//             ),
-//             0.2 ether
-//         );
-//     }
+        startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        depositInstance.deposit{value: 0.1 ether}();
+        assertEq(
+            depositInstance.depositorBalances(
+                0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931
+            ),
+            0.2 ether
+        );
+    }
 
-//     function testDepositDisablesBidding() public {
-//         startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         depositInstance.deposit{value: 0.1 ether}();
-//         assertEq(auctionInstance.bidsEnabled(), false);        
-//     }
+    function testDepositDisablesBidding() public {
+        startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        depositInstance.deposit{value: 0.1 ether}();
+        assertEq(auctionInstance.bidsEnabled(), false);        
+    }
 
-//     function testDepositFailsIfIncorrectAmountSent() public {
-//         startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-//         vm.expectRevert("Insufficient staking amount");
-//         depositInstance.deposit{value: 0.2 ether}();
-//     }
-// }
+    function testDepositFailsIfIncorrectAmountSent() public {
+        startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        vm.expectRevert("Insufficient staking amount");
+        depositInstance.deposit{value: 0.2 ether}();
+    }
+
+    function _merkleSetup() internal {
+        merkle = new Merkle();
+
+        whiteListedAddresses.push(keccak256(abi.encodePacked(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931)));
+        whiteListedAddresses.push(keccak256(abi.encodePacked(0x9154a74AAfF2F586FB0a884AeAb7A64521c64bCf)));
+        whiteListedAddresses.push(keccak256(abi.encodePacked(0xCDca97f61d8EE53878cf602FF6BC2f260f10240B)));
+
+        root = merkle.getRoot(whiteListedAddresses);
+    }
+}
