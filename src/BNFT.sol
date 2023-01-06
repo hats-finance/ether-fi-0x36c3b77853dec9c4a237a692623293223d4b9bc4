8f1f601f683541bdadad276e057af158159af3df
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-06 17:08:07 +0200

    Added natspec to BNFT.sol

--- src/BNFT.sol
+++ src/BNFT.sol
@@ -4,17 +4,29 @@ pragma solidity 0.8.13;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BNFT is ERC721 {
+
+//--------------------------------------------------------------------------------------
+//---------------------------------  STATE-VARIABLES  ----------------------------------
+//--------------------------------------------------------------------------------------
+  
     uint256 private tokenIds;
     uint256 public nftValue = 2 ether;
-
     address public depositContractAddress;
     address public owner;
 
+//--------------------------------------------------------------------------------------
+//----------------------------------  CONSTRUCTOR   ------------------------------------
+//--------------------------------------------------------------------------------------
+   
     constructor(address _owner) ERC721("Bond NFT", "BNFT") {
         depositContractAddress = msg.sender;
         owner = _owner;
     }
 
+//--------------------------------------------------------------------------------------
+//----------------------------  STATE-CHANGING FUNCTIONS  ------------------------------
+//--------------------------------------------------------------------------------------
+    
     function mint(address _reciever) external onlyDepositContract {
         _safeMint(_reciever, tokenIds);
         unchecked {
@@ -30,6 +42,10 @@ contract BNFT is ERC721 {
         require(from == address(0), "Err: token is SOUL BOUND");
         super.transferFrom(from, to, tokenId);
     }
+
+//--------------------------------------------------------------------------------------
+//-----------------------------------  MODIFIERS  --------------------------------------
+//--------------------------------------------------------------------------------------
 
     modifier onlyDepositContract() {
         require(
