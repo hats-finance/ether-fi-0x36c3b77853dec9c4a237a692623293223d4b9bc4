f9bac5da7639e1b1468414f6e5296977a322073d
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-06 18:22:37 +0200

    Finished comments

--- src/Auction.sol
+++ src/Auction.sol
@@ -24,6 +24,7 @@ contract Auction is IAuction {
     address public treasuryContractAddress;
     bytes32 public merkleRoot;
     bool public bidsEnabled;
+    bool public depositContractAddressSet;
 
     mapping(uint256 => Bid) public bids;
 
@@ -42,6 +43,9 @@ contract Auction is IAuction {
 //----------------------------------  CONSTRUCTOR   ------------------------------------
 //--------------------------------------------------------------------------------------
     
+    /// @notice Constructor to set variables on deployment
+    /// @param _treasuryAddress the address of the treasury to send funds to
+    /// @param _merkleRoot the merkle root which holds the whitelisted addresses for bidding
     constructor(address _treasuryAddress, bytes32 _merkleRoot) {
         bidsEnabled = true;
         treasuryContractAddress = _treasuryAddress;
@@ -52,15 +56,24 @@ contract Auction is IAuction {
 //----------------------------  STATE-CHANGING FUNCTIONS  ------------------------------
 //--------------------------------------------------------------------------------------
     
+    /// @notice Disables the bidding to prevent race-conditions on arrival of a stake
+    /// @dev Used local variables to prevent multiple calling of state variables to save gas
+    /// @dev Gets called from the deposit contract when a stake is received
+    /// @return winningOperator the address of the current highest bidder
     function disableBidding() external onlyDepositContract returns (address) {
         uint256 currentHighestBidIdLocal = currentHighestBidId;
         uint256 numberOfBidsLocal = numberOfBids;
+        
+        //Disable bids to prevent race-conditions
         bidsEnabled = false;
+
+        //Set the bid to be de-activated to prevent 1 bid winning twice
         bids[currentHighestBidIdLocal].isActive = false;
         address winningOperator = bids[currentHighestBidIdLocal].bidderAddress;
         uint256 winningBidAmount = bids[currentHighestBidIdLocal].amount;
         uint256 tempWinningBidId;
 
+        //Loop to calculate the next highest bid for the next stake
         for (uint256 x; x <= numberOfBidsLocal - 1; ++x) {
             if (
                 (bids[x].isActive == true) &&
@@ -69,9 +82,9 @@ contract Auction is IAuction {
                 tempWinningBidId = x;
             }
         }
-
         currentHighestBidId = tempWinningBidId;
 
+        //Send the winning bid to the treasury contract
         (bool sent, ) = treasuryContractAddress.call{value: winningBidAmount}(
             ""
         );
@@ -83,18 +96,24 @@ contract Auction is IAuction {
         return winningOperator;
     }
 
+    /// @notice Enables the bidding
+    /// @dev Currently must get called manually for POC
+    /// @dev Will be called from deposit contract when validator key is sent
     function enableBidding() external onlyDepositContract {
         require(bidsEnabled == false, "Bids already enabled");
         bidsEnabled = true;
         emit BiddingEnabled();
     }
 
+    /// @notice Increases a currently active bid by a specified amount
+    /// @param _bidId the ID of the bid to increase
     function updateBid(uint256 _bidId) external payable {
         require(bids[_bidId].bidderAddress == msg.sender, "Invalid bid");
         require(bids[_bidId].isActive == true, "Bid already cancelled");
 
         bids[_bidId].amount += msg.value;
 
+        //Checks if the updated amount is now the current highest bid
         if (bids[_bidId].amount > bids[currentHighestBidId].amount) {
             currentHighestBidId = _bidId;
         }
@@ -102,17 +121,26 @@ contract Auction is IAuction {
         emit BidUpdated(_bidId, msg.value);
     }
 
+    /// @notice Cancels a specified bid by de-activating it
+    /// @dev Used local variables to save on multiple state variable lookups
+    /// @dev First require checks both if the bid doesnt exist and if its called by incorrect owner
+    /// @param _bidId the ID of the bid to cancel
     function cancelBid(uint256 _bidId) external {
         require(bids[_bidId].bidderAddress == msg.sender, "Invalid bid");
         require(bids[_bidId].isActive == true, "Bid already cancelled");
 
+        //Set local variable for read operations to save gas
         uint256 numberOfBidsLocal = numberOfBids;
 
+        //Cancel the bid by de-activating it
         bids[_bidId].isActive = false;
 
+        //Check if the bid being cancelled is the current highest to make sure we
+        //calculate a new highest
         if (currentHighestBidId == _bidId) {
             uint256 tempWinningBidId;
 
+            //Calculate the new highest bid
             for (uint256 x; x <= numberOfBidsLocal - 1; ++x) {
                 if (
                     (bids[x].amount > bids[tempWinningBidId].amount) &&
@@ -125,8 +153,10 @@ contract Auction is IAuction {
             currentHighestBidId = tempWinningBidId;
         }
 
+        //Get the value of the cancelled bid to refund
         uint256 bidValue = bids[_bidId].amount;
 
+        //Refund the user with their bid amount
         (bool sent, ) = msg.sender.call{value: bidValue}("");
         require(sent, "Failed to send Ether");
 
@@ -135,6 +165,9 @@ contract Auction is IAuction {
         emit BidCancelled(_bidId);
     }
 
+    /// @notice Places a bid in the auction to be the next operator
+    /// @dev Merkleroot gets generated in JS offline and sent to the contract
+    /// @param _merkleProof the merkleproof for the user calling the function
     function bidOnStake(bytes32[] calldata _merkleProof) external payable {
         require(bidsEnabled == true, "Bidding is on hold");
         require(
@@ -146,6 +179,7 @@ contract Auction is IAuction {
             "Invalid merkle proof"
         );
 
+        //Creates a bid object for storage and lookup in future
         bids[numberOfBids] = Bid({
             amount: msg.value,
             timeOfBid: block.timestamp,
@@ -153,6 +187,7 @@ contract Auction is IAuction {
             isActive: true
         });
 
+        //Checks if the bid is now the highest bid
         if (msg.value > bids[currentHighestBidId].amount) {
             currentHighestBidId = numberOfBids;
         }
@@ -163,6 +198,9 @@ contract Auction is IAuction {
         emit BidPlaced(msg.sender, msg.value, numberOfBids - 1);
     }
 
+    /// @notice Updates the merkle root whitelists have been updated
+    /// @dev erkleroot gets generated in JS offline and sent to the contract
+    /// @param _newMerkle new merkle root to be used for bidding
     function updateMerkleRoot(bytes32 _newMerkle) external {
         bytes32 oldMerkle = merkleRoot;
         merkleRoot = _newMerkle;
@@ -170,16 +208,26 @@ contract Auction is IAuction {
         emit MerkleUpdated(oldMerkle, _newMerkle);
     }
 
+    /// @notice Sets the depositContract address in the current contract
+    /// @dev Called by depositContract and can only be called once
+    /// @param _depositContractAddress address of the depositContract for authorizations
     function setDepositContractAddress(address _depositContractAddress)
         external
     {
+        require(depositContractAddressSet == false, "Function already called");
         depositContractAddress = _depositContractAddress;
+        
+        //Setting boolean to true to stop it from being called again
+        depositContractAddressSet = true;
     }
 
 //--------------------------------------------------------------------------------------
 //-------------------------------------  GETTER   --------------------------------------
 //--------------------------------------------------------------------------------------
 
+    /// @notice Fetches how many active bids there are and sends it to the caller
+    /// @dev Needed for deposit to check if there are any active bids
+    /// @return numberOfActiveBids the number of current active bids
     function getNumberOfActivebids() external view returns (uint256) {
         return numberOfActiveBids;
     }
