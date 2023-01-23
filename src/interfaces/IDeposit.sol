3af05fa8e65b494bd85579341334fb83bfcf4f77
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-01-23 15:04:13 +0200

    Initial

--- src/interfaces/IDeposit.sol
+++ src/interfaces/IDeposit.sol
@@ -2,6 +2,14 @@
 pragma solidity 0.8.13;
 
 interface IDeposit {
+
+    struct Stake {
+        address staker,
+        bytes32 withdrawCredentials,
+        uint256 amount,
+        STAKE_PHASE phase
+    }
+
     function deposit() external payable;
 
     function setStakeAmount(uint256 _newStakeAmount) external;
