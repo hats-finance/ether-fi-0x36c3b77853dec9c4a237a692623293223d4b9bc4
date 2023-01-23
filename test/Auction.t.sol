9240c920a03535f7e3606c62b6a9366a759a1d36
Myles Traut <myles@linumlabs.com>
2023-01-23 11:15:22 +0200

    added test_EventMinBidUpdated

--- test/Auction.t.sol
+++ test/Auction.t.sol
@@ -22,6 +22,11 @@ contract AuctionTest is Test {
     address owner = vm.addr(1);
     address alice = vm.addr(2);
 
+    event MinBidUpdated(
+        uint256 indexed oldMinBidAmount,
+        uint256 indexed newMinBidAmount
+    );
+
     function setUp() public {
         vm.startPrank(owner);
         treasuryInstance = new Treasury();
@@ -576,6 +581,13 @@ contract AuctionTest is Test {
         vm.prank(owner);
         auctionInstance.setMinBidPrice(1 ether);
         assertEq(auctionInstance.minBidAmount(), 1 ether);
+    }
+
+    function test_EventMinBidUpdated() public {
+        vm.expectEmit(true, true, false, true);
+        emit MinBidUpdated(0, 1 ether);
+        vm.prank(owner);
+        auctionInstance.setMinBidPrice(1 ether);
     }
 
     function _merkleSetup() internal {
