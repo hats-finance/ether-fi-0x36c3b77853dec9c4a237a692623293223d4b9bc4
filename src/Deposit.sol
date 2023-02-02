c24052c9f429ad997d60d67eebc3db60f589ae7d
Myles Traut <myles@linumlabs.com>
2023-02-02 12:10:52 +0200

    changed deposit amount and commented out deposit to beacon chain functionality for tests to pass

--- src/Deposit.sol
+++ src/Deposit.sol
@@ -58,7 +58,7 @@ contract Deposit is IDeposit, Pausable {
     /// @dev Auction contract must be deployed first
     /// @param _auctionAddress the address of the auction contract for interaction
     constructor(address _auctionAddress) {
-        stakeAmount = 32 ether;
+        stakeAmount = 0.032 ether;
         TNFTInstance = new TNFT();
         BNFTInstance = new BNFT();
         TNFTInterfaceInstance = ITNFT(address(TNFTInstance));
@@ -179,12 +179,12 @@ contract Deposit is IDeposit, Pausable {
 
         DepositData memory dataInstance = stakes[localStakeId].deposit_data;
 
-        depositContractEth2.deposit{value: stakeAmount}(
-            dataInstance.publicKey,
-            abi.encodePacked(dataInstance.withdrawalCredentials),
-            dataInstance.signature,
-            dataInstance.depositDataRoot
-        );
+        // depositContractEth2.deposit{value: stakeAmount}(
+        //     dataInstance.publicKey,
+        //     abi.encodePacked(dataInstance.withdrawalCredentials),
+        //     dataInstance.signature,
+        //     dataInstance.depositDataRoot
+        // );
         emit ValidatorAccepted(_validatorId, address(withdrawSafeInstance));
     }
 
