cf42f134349812c03d7d21ad885d7464aea110fa
Myles Traut <myles@linumlabs.com>
2023-02-03 13:42:08 +0200

    updated test_LargeScenario to pass

--- test/LargeScenario.t.sol
+++ test/LargeScenario.t.sol
@@ -246,10 +246,10 @@ contract LargeScenariosTest is Test {
         hoax(0x835ff0CC6F35B148b85e0E289DAeA0497ec5aA7f);
         depositInstance.deposit{value: 0.032 ether}();
 
-        assertEq(auctionInstance.currentHighestBidId(), 4);
+        assertEq(auctionInstance.currentHighestBidId(), 1);
         assertEq(auctionInstance.numberOfActiveBids(), 1);
-        assertEq(address(treasuryInstance).balance, 1.7 ether);
-        assertEq(address(auctionInstance).balance, 0.4 ether);
+        assertEq(address(treasuryInstance).balance, 1.1 ether);
+        assertEq(address(auctionInstance).balance, 0.1 ether);
         assertEq(address(depositInstance).balance, 0.064 ether);
 
         //Deposit One cancelled
@@ -259,8 +259,8 @@ contract LargeScenariosTest is Test {
         assertEq(auctionInstance.currentHighestBidId(), 3);
         assertEq(auctionInstance.numberOfBids() - 1, 4);
         assertEq(auctionInstance.numberOfActiveBids(), 2);
-        assertEq(address(treasuryInstance).balance, 1 ether);
-        assertEq(address(auctionInstance).balance, 1.1 ether);
+        assertEq(address(treasuryInstance).balance, 0.4 ether);
+        assertEq(address(auctionInstance).balance, 0.8 ether);
         assertEq(address(depositInstance).balance, 0.032 ether);
 
         //Deposit Two register validator
@@ -282,7 +282,7 @@ contract LargeScenariosTest is Test {
 
         ) = depositInstance.validators(0);
         assertEq(validatorId, 0);
-        assertEq(bidId, 1);
+        assertEq(bidId, 4);
         assertEq(stakeId, 1);
         assertEq(validatorKey, "Encrypted_Key");
         assertEq(encryptedValidatorKeyPassword, "encrypted_key_password");
@@ -297,7 +297,7 @@ contract LargeScenariosTest is Test {
         vm.expectRevert("Incorrect caller");
         depositInstance.acceptValidator(0);
 
-        hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        hoax(0x48809A2e8D921790C0B8b977Bbb58c5DbfC7f098);
         depositInstance.acceptValidator(0);
         assertEq(
             TestBNFTInstance.ownerOf(0),
