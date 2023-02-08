32a2fa8bb9fa0b560143d6d74c186fc2f5d840e0
Myles Traut <myles@linumlabs.com>
2023-02-08 11:10:49 +0200

    removed refundBid call from reEnterAuction function

--- src/Auction.sol
+++ src/Auction.sol
@@ -301,7 +301,6 @@ contract Auction is IAuction, Pausable {
 
         //Reactivate the bid
         bids[_bidId].isActive = true;
-        // withdrawSafeInstance.refundBid(bids[_bidId].amount, _bidId);
 
         //Checks if the bid is now the highest bid
         if (bids[_bidId].amount > bids[currentHighestBidId].amount) {
