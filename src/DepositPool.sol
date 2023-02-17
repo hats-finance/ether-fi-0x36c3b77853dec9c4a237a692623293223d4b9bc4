311c5447abad1d9fcc8cd8752c44adf6a00984a2
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-02-17 14:28:47 +0200

    Testing deposit and withdraw

--- src/DepositPool.sol
+++ src/DepositPool.sol
@@ -2,12 +2,14 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "lib/forge-std/src/console.sol";
 
 contract DepositPool {
     //--------------------------------------------------------------------------------------
     //---------------------------------  STATE-VARIABLES  ----------------------------------
     //--------------------------------------------------------------------------------------
-    uint256 depositStandard = 100000000000000000;
+    uint256 public constant depositStandard = 100000000000000000;
+    uint256 public constant SCALE = 100;
     mapping(address => uint256) public depositTimes;
     mapping(address => uint256) public userBalance;
     mapping(address => uint256) public userPoints;
@@ -17,12 +19,12 @@ contract DepositPool {
     //--------------------------------------------------------------------------------------
 
     event Deposit(address indexed sender, uint256 amount);
-    event Withdraw(address indexed sender, uint256 amount);
+    event Withdraw(address indexed sender, uint256 amount, uint256 lengthOfDeposit);
 
     //--------------------------------------------------------------------------------------
     //----------------------------------  CONSTRUCTOR   ------------------------------------
     //--------------------------------------------------------------------------------------
-
+    
     //--------------------------------------------------------------------------------------
     //----------------------------  STATE-CHANGING FUNCTIONS  ------------------------------
     //--------------------------------------------------------------------------------------
@@ -30,7 +32,6 @@ contract DepositPool {
     /// @notice deposit into pool
     function deposit() external payable {
         require(msg.value >= 0.1 ether, "Deposit too small");
-
         depositTimes[msg.sender] = block.timestamp;
         userBalance[msg.sender] = msg.value;
 
@@ -40,22 +41,26 @@ contract DepositPool {
     /// @notice withdraw from pool
     function withdraw() public payable {
 
-        uint256 lengthOfWithdrawal = block.timestamp - depositTimes[msg.sender];
+        uint256 lengthOfDeposit = block.timestamp - depositTimes[msg.sender];
         uint256 balance = userBalance[msg.sender];
 
         depositTimes[msg.sender] = 0;
         userBalance[msg.sender] = 0;
-        userPoints[msg.sender] += calculateUserPoints(balance, lengthOfWithdrawal);
+        userPoints[msg.sender] += calculateUserPoints(balance, lengthOfDeposit);
 
         (bool sent, ) = msg.sender.call{value: balance}("");
         require(sent, "Failed to send Ether");
         
-        emit Withdraw(msg.sender, msg.value);
+        emit Withdraw(msg.sender, msg.value, lengthOfDeposit);
     }
 
     function calculateUserPoints(uint256 _depositAmount, uint256 _numberOfSeconds) internal view returns (uint256) {
 
-        uint256 numberOfDepositStandards = _depositAmount / depositStandard;
-        return numberOfDepositStandards * _numberOfSeconds;
+        uint256 numberOfDepositStandards = (_depositAmount * SCALE) / depositStandard;
+        return (numberOfDepositStandards * _numberOfSeconds) / SCALE;
+    }
+
+    /// @notice Allows ether to be sent to this contract
+    receive() external payable {
     }
 }
---
+++ test/DepositPool.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "../src/DepositPool.sol";
+
+contract DepositPoolTest is Test {
+    
+    DepositPool depositPoolInstance;
+    
+    address owner = vm.addr(1);
+    address alice = vm.addr(2);
+
+    function setUp() public {
+        vm.startPrank(owner);
+        depositPoolInstance = new DepositPool();
+        vm.stopPrank();
+    }
+
+    function test_DepositPoolWorksCorrectly() public {
+        startHoax(owner);
+        depositPoolInstance.deposit{value: 0.3 ether}();
+        assertEq(depositPoolInstance.userBalance(owner), 0.3 ether);
+        assertEq(depositPoolInstance.depositTimes(owner), 1);
+        vm.warp(51);
+        depositPoolInstance.withdraw();
+        assertEq(depositPoolInstance.userBalance(owner), 0 ether);
+        assertEq(depositPoolInstance.userPoints(owner), 150);
+        assertEq(depositPoolInstance.depositTimes(owner), 0);
+        vm.stopPrank();
+
+        startHoax(alice);
+        depositPoolInstance.deposit{value: 1.65 ether}();
+        assertEq(depositPoolInstance.userBalance(alice), 1.65 ether);
+        assertEq(depositPoolInstance.depositTimes(alice), 51);
+        vm.warp(1305);
+        depositPoolInstance.withdraw();
+        assertEq(depositPoolInstance.userBalance(alice), 0 ether);
+        assertEq(depositPoolInstance.userPoints(alice), 20691);
+        assertEq(depositPoolInstance.depositTimes(alice), 0);
+        vm.stopPrank();
+    }
+
+}
