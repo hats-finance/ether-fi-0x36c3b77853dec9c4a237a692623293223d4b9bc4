06136a900d334b88cb7fc33ef1d4fb88ed75b6c9
Myles Traut <myles@linumlabs.com>
2023-02-18 18:38:03 +0200

    added deposit script

--- src/DepositPool.sol
+++ src/DepositPool.sol
@@ -4,7 +4,8 @@ pragma solidity 0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import "lib/forge-std/src/console.sol";
+
+// import "lib/forge-std/src/console.sol";
 
 contract DepositPool is Ownable {
     using Math for uint256;
