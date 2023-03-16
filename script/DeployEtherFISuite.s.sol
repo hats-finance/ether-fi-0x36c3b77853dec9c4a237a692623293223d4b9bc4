57f3084dabad0cc865e78b57914c697fef8e562c
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-03-16 18:23:13 +0200

    Compile issues

--- script/DeployEtherFISuite.s.sol
+++ script/DeployEtherFISuite.s.sol
@@ -54,10 +54,6 @@ contract DeployScript is Script {
             BNFTAddress
         );
 
-
-        auctionManager.setEtherFiNodesManagerAddress(
-            address(etherFiNodesManager)
-        );
         stakingManager.setEtherFiNodesManagerAddress(
             address(etherFiNodesManager)
         );
