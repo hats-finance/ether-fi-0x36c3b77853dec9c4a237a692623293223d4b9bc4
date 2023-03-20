1ae21de9cdc78a103606433d1cb4db9f51986ba9
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-03-20 11:14:34 +0200

    Staging

--- src/ConversionPool.sol
+++ src/ConversionPool.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
+pragma abicoder v2;
+
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -12,6 +15,7 @@ contract ConversionPool is Ownable, ReentrancyGuard, Pausable {
     //---------------------------------  STATE-VARIABLES  ----------------------------------
     //--------------------------------------------------------------------------------------
 
+    ISwapRouter public immutable swapRouter;
 
     //--------------------------------------------------------------------------------------
     //-------------------------------------  EVENTS  ---------------------------------------
@@ -25,14 +29,34 @@ contract ConversionPool is Ownable, ReentrancyGuard, Pausable {
     //----------------------------------  CONSTRUCTOR   ------------------------------------
     //--------------------------------------------------------------------------------------
 
-    constructor(
-    ) {
-        
+    constructor(address _routerAddress) {
+        swapRouter = ISwapRouter(_routerAddress);
     }
 
     //--------------------------------------------------------------------------------------
     //----------------------------  STATE-CHANGING FUNCTIONS  ------------------------------
     //--------------------------------------------------------------------------------------
+
+    function swapExactInputSingle(uint256 amountIn)
+        external
+        returns (uint256 amountOut)
+    {
+        linkToken.approve(address(swapRouter), amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: LINK,
+                tokenOut: WETH,
+                fee: poolFee,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        amountOut = swapRouter.exactInputSingle(params);
+    }
 
     //Pauses the contract
     function pauseContract() external onlyOwner {
