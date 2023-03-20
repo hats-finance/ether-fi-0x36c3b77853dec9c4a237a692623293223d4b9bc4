c4a03a04cabdbfccb17c88b02e407ced2cf2271e
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-03-20 15:20:53 +0200

    Initial contract with dynamic swap and withdraw functions

--- src/ConversionPool.sol
+++ src/ConversionPool.sol
@@ -2,28 +2,41 @@
 pragma solidity 0.8.13;
 pragma abicoder v2;
 
-import "@uniswap/contracts/interfaces/ISwapRouter.sol";
-
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract ConversionPool is Ownable, ReentrancyGuard, Pausable {
-   
+    
+    using SafeERC20 for IERC20;
+
 
     //--------------------------------------------------------------------------------------
     //---------------------------------  STATE-VARIABLES  ----------------------------------
     //--------------------------------------------------------------------------------------
 
+    uint24 public constant poolFee = 3000;
+
+    address private immutable rETH = 0xae78736Cd615f374D3085123A210448E74Fc6393;
+    address private immutable wstETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+    address private immutable sfrxETH = 0xac3E018457B222d93114458476f3E3416Abbe38F;
+    address private immutable cbETH = 0xBe9895146f7AF43049ca1c1AE358B0541Ea49704;
+    address private immutable wEth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
     ISwapRouter public immutable swapRouter;
+    IERC20 public rETHInstance;
+    IERC20 public wstETHInstance;
+    IERC20 public sfrxETHInstance;
+    IERC20 public cbETHInstance;
+
+    mapping(address => mapping(address => uint256)) public finalUserToErc20Balance;
 
     //--------------------------------------------------------------------------------------
     //-------------------------------------  EVENTS  ---------------------------------------
     //--------------------------------------------------------------------------------------
-
-
-    /// @notice Allows ether to be sent to this contract
-    receive() external payable {}
 
     //--------------------------------------------------------------------------------------
     //----------------------------------  CONSTRUCTOR   ------------------------------------
@@ -31,31 +44,38 @@ contract ConversionPool is Ownable, ReentrancyGuard, Pausable {
 
     constructor(address _routerAddress) {
         swapRouter = ISwapRouter(_routerAddress);
+        rETHInstance = IERC20(0xae78736Cd615f374D3085123A210448E74Fc6393);
+        wstETHInstance = IERC20(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0);
+        sfrxETHInstance = IERC20(0xac3E018457B222d93114458476f3E3416Abbe38F);
+        cbETHInstance = IERC20(0xBe9895146f7AF43049ca1c1AE358B0541Ea49704);
     }
 
     //--------------------------------------------------------------------------------------
     //----------------------------  STATE-CHANGING FUNCTIONS  ------------------------------
     //--------------------------------------------------------------------------------------
 
-    function swapExactInputSingle(uint256 amountIn)
-        external
-        returns (uint256 amountOut)
-    {
-        linkToken.approve(address(swapRouter), amountIn);
+    function withdrawERC20() external {
+        uint256 rETHbal = finalUserToErc20Balance[msg.sender][rETH];
+        uint256 wstETHbal = finalUserToErc20Balance[msg.sender][wstETH];
+        uint256 sfrxEthbal = finalUserToErc20Balance[msg.sender][sfrxETH];
+        uint256 cbEthBal = finalUserToErc20Balance[msg.sender][cbETH];
 
-        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
-            .ExactInputSingleParams({
-                tokenIn: LINK,
-                tokenOut: WETH,
-                fee: poolFee,
-                recipient: address(this),
-                deadline: block.timestamp,
-                amountIn: amountIn,
-                amountOutMinimum: 0,
-                sqrtPriceLimitX96: 0
-            });
+        finalUserToErc20Balance[msg.sender][rETH] = 0;
+        finalUserToErc20Balance[msg.sender][wstETH] = 0;
+        finalUserToErc20Balance[msg.sender][sfrxETH] = 0;
+        finalUserToErc20Balance[msg.sender][cbETH] = 0;
 
-        amountOut = swapRouter.exactInputSingle(params);
+        rETHInstance.safeTransfer(msg.sender, rETHbal);
+        wstETHInstance.safeTransfer(msg.sender, wstETHbal);
+        sfrxETHInstance.safeTransfer(msg.sender, sfrxEthbal);
+        cbETHInstance.safeTransfer(msg.sender, cbEthBal);
+    }
+
+    function sendTokensToLP() external {
+        //Swap ERC20 for eth
+        //Use internal swap function setup
+
+        //Call function in LP and send in user and amount of ether sent
     }
 
     //Pauses the contract
@@ -71,6 +91,27 @@ contract ConversionPool is Ownable, ReentrancyGuard, Pausable {
     //--------------------------------------------------------------------------------------
     //--------------------------------  INTERNAL FUNCTIONS  --------------------------------
     //--------------------------------------------------------------------------------------
+
+    function _swapExactInputSingle(uint256 _amountIn, address _tokenIn)
+        internal
+        returns (uint256 amountOut)
+    {
+        IERC20(_tokenIn).approve(address(swapRouter), _amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: _tokenIn,
+                tokenOut: wEth,
+                fee: poolFee,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: _amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        amountOut = swapRouter.exactInputSingle(params);
+    }
 
     //--------------------------------------------------------------------------------------
     //-------------------------------------     GETTERS  -----------------------------------
