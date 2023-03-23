f8be00e97061c02a3c8e9aee2801a97184bf4a30
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-03-23 15:15:47 +0200

    Merged master

--- test/NodeOperatorManager.t.sol
+++ test/NodeOperatorManager.t.sol
@@ -148,13 +148,10 @@ contract NodeOperatorManagerTest is Test {
         auctionInstance.createBid{value: 0.2 ether}(2, 0.1 ether);
         vm.stopPrank();
 
-        vm.expectRevert("Only auction contract function");
+        vm.expectRevert("Only auction manager contract function");
         vm.prank(alice);
         nodeOperatorManagerInstance.fetchNextKeyIndex(alice);
     }
-
-    
-
 
     function test_UpdatingMerkle() public {
         assertEq(nodeOperatorManagerInstance.merkleRoot(), root);
