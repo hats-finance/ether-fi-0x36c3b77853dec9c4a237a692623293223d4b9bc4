a61960a15082caecd7656f94bbd9d5dedde94965
ReposCollector <seongyun@postech.ac.kr>
2023-03-23 20:55:59 -0500

    treasury withdraw takes the address to send the fund to

--- src/Treasury.sol
+++ src/Treasury.sol
@@ -12,9 +12,9 @@ contract Treasury is ITreasury, Ownable {
     //--------------------------------------------------------------------------------------
 
     /// @notice Function allows only the owner to withdraw all the funds in the contract
-    function withdraw(uint256 _amount) external onlyOwner {
+    function withdraw(uint256 _amount, address _to) external onlyOwner {
         require(_amount <= address(this).balance, "the balance is lower than the requested amount");
-        (bool sent, ) = msg.sender.call{value: _amount}("");
+        (bool sent, ) = payable(_to).call{value: _amount}("");
         require(sent, "Failed to send Ether");
     }
 
