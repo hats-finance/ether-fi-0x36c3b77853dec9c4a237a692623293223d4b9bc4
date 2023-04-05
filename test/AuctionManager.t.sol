563322e9acd4a7f7735e92df64fbefa8ff77fac1
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-04-05 09:50:36 +0200

    Fixed auction tests

--- test/AuctionManager.t.sol
+++ test/AuctionManager.t.sol
@@ -735,37 +735,53 @@ contract AuctionManagerTest is TestSetup {
         assertEq(auctionInstance.numberOfActiveBids(), 1);
     }
 
-    // function test_ProcessAuctionFeeTransfer() public {
-    //     bytes32[] memory proofAddress1 = merkle.getProof(
-    //         whiteListedAddresses,
-    //         0
-    //     );
+    function test_ProcessAuctionFeeTransfer() public {
+        bytes32[] memory proofAddress1 = merkle.getProof(
+            whiteListedAddresses,
+            0
+        );
 
-    //     vm.prank(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-    //     nodeOperatorManagerInstance.registerNodeOperator(
-    //         proofAddress1,
-    //         _ipfsHash,
-    //         5
-    //     );
+        vm.prank(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        nodeOperatorManagerInstance.registerNodeOperator(
+            proofAddress1,
+            _ipfsHash,
+            5
+        );
 
-    //     hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-    //     uint256[] memory bid1Ids = auctionInstance.createBid{value: 1 ether}(
-    //         1,
-    //         1 ether
-    //     );
+        hoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        uint256[] memory bid1Ids = auctionInstance.createBid{value: 1 ether}(
+            1,
+            1 ether
+        );
 
-    //     vm.prank(owner);
-    //     vm.expectRevert("Only staking manager contract function");
-    //     auctionInstance.processAuctionFeeTransfer(bid1Ids[0]);
+        vm.prank(owner);
+        vm.expectRevert("Only staking manager contract function");
+        auctionInstance.processAuctionFeeTransfer(bid1Ids[0]);
 
-    //     startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
-    //     uint256[] memory processedBidIds = stakingManagerInstance.batchDepositWithBidIds{value: 32 ether}(bid1Ids);
+        startHoax(0xCd5EBC2dD4Cb3dc52ac66CEEcc72c838B40A5931);
+        uint256[] memory processedBidIds = stakingManagerInstance.batchDepositWithBidIds{value: 32 ether}(bid1Ids);
 
-    //     stakingManagerInstance.registerValidator(processedBidIds[0], test_data);
+        address etherFiNode = managerInstance.etherfiNodeAddress(1);
+        bytes32 root = depGen.generateDepositRoot(
+            hex"8f9c0aab19ee7586d3d470f132842396af606947a0589382483308fdffdaf544078c3be24210677a9c471ce70b3b4c2c",
+            hex"877bee8d83cac8bf46c89ce50215da0b5e370d282bb6c8599aabdbc780c33833687df5e1f5b5c2de8a6cd20b6572c8b0130b1744310a998e1079e3286ff03e18e4f94de8cdebecf3aaac3277b742adb8b0eea074e619c20d13a1dda6cba6e3df",
+            managerInstance.generateWithdrawalCredentials(etherFiNode),
+            32 ether
+        );
 
-    //     address safe = managerInstance.etherfiNodeAddress(processedBidIds[0]);
-    //     assertEq(safe.balance, 0.5 ether);
-    // }
+        IStakingManager.DepositData memory depositData = IStakingManager
+            .DepositData({
+                publicKey: hex"8f9c0aab19ee7586d3d470f132842396af606947a0589382483308fdffdaf544078c3be24210677a9c471ce70b3b4c2c",
+                signature: hex"877bee8d83cac8bf46c89ce50215da0b5e370d282bb6c8599aabdbc780c33833687df5e1f5b5c2de8a6cd20b6572c8b0130b1744310a998e1079e3286ff03e18e4f94de8cdebecf3aaac3277b742adb8b0eea074e619c20d13a1dda6cba6e3df",
+                depositDataRoot: root,
+                ipfsHashForEncryptedValidatorKey: "test_ipfs"
+            });
+
+
+        stakingManagerInstance.registerValidator(processedBidIds[0], depositData);
+
+        assertEq(etherFiNode.balance, 0.5 ether);
+    }
 
     function test_SetMaxBidAmount() public {
         vm.prank(owner);
