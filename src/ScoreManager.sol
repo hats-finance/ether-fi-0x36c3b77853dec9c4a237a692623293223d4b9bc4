fd75246f3ce542070cfd9241095fd678f7c6af48
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-04-07 17:12:39 +0200

    Added skeleton

---
+++ src/ScoreManager.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import "@openzeppelin-upgradeable/contracts/security/PausableUpgradeable.sol";
+import "@openzeppelin-upgradeable/contracts/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+contract StakingManager is
+    Initializable,
+    OwnableUpgradeable,
+    PausableUpgradeable,
+    ReentrancyGuardUpgradeable,
+    UUPSUpgradeable
+{
+    // string: indicate the type of the score (like the name of the promotion)
+    // address: user wallet address
+    // bytes256: a byte stream of user score + etc
+    mapping(string => mapping(address => bytes)) scores;
+    mapping(address => bool) allowedCallers;
+
+    uint256[32] __gap;
+
+    //--------------------------------------------------------------------------------------
+    //-------------------------------------  EVENTS  ---------------------------------------
+    //--------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------
+    //----------------------------  STATE-CHANGING FUNCTIONS  ------------------------------
+    //--------------------------------------------------------------------------------------
+
+    /// @notice initialize to set variables on deployment
+    /// @dev Deploys NFT contracts internally to ensure ownership is set to this contract
+    /// @dev AuctionManager contract must be deployed first
+    function initialize() external initializer {
+        __Pausable_init();
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+        __ReentrancyGuard_init();
+    }
+
+    /// @notice fetches the score in bytes form for a specific user and for a specific reward category
+    /// @param _name the name of the category
+    /// @param _user the user to fetch the score for
+    /// @return the score the user has
+    function get(
+        string memory _name,
+        address _user
+    ) external view returns (bytes memory) {
+        return scores[_name][_user];
+    }
+
+    /// @notice sets the score of a user
+    /// @dev will be called by approved contracts that can set reward totals
+    /// @param _name the name of the category
+    /// @param _user the user to fetch the score for
+    /// @param _score the score the user will receive in bytes form
+    function set(
+        string memory _name,
+        address _user,
+        bytes memory _score
+    ) external allowedCaller(msg.sender) {
+        scores[_name][_user] = _score;
+    }
+
+    /// @notice updates the status of a caller
+    /// @param _caller the address of the contract or EOA that is being updated
+    /// @param _flag true or false value to update their calling status
+    function setCallerStatus(address _caller, bool _flag) external onlyOwner {
+        allowedCallers[_caller] = _flag;
+    }
+
+    //--------------------------------------------------------------------------------------
+    //-------------------------------  INTERNAL FUNCTIONS   --------------------------------
+    //--------------------------------------------------------------------------------------
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
+
+    //--------------------------------------------------------------------------------------
+    //------------------------------------  GETTERS  ---------------------------------------
+    //--------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------
+    //-----------------------------------  MODIFIERS  --------------------------------------
+    //--------------------------------------------------------------------------------------
+
+    modifier allowedCaller(address _caller) {
+        require(allowedCallers[_caller], "now allowed to call");
+        _;
+    }
+}
