128e58c011b5cc9a7af8a22f03a30e18942c1c02
ReposCollector <seongyun@postech.ac.kr>
2023-04-09 20:05:23 -0500

    implement basic implementation of liquidity pool and eETH

--- src/EETH.sol
+++ src/EETH.sol
@@ -1,18 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "@openzeppelin-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol";
 
 import "./interfaces/IEETH.sol";
+import "./interfaces/ILiquidityPool.sol";
 
-contract EETH is ERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable, IEETH {
+contract EETH is IERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable, IEETH {
     //--------------------------------------------------------------------------------------
     //---------------------------------  STATE-VARIABLES  ----------------------------------
     //--------------------------------------------------------------------------------------
 
-    address public liquidityPool;
+    ILiquidityPool public liquidityPool;
+
+    uint256 public totalShares;
+    mapping (address => uint256) public shares;
+    mapping (address => mapping (address => uint256)) public allowances;
 
     uint256[32] __gap;
 
@@ -20,28 +25,104 @@ contract EETH is ERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable, IEETH {
     //----------------------------  STATE-CHANGING FUNCTIONS  ------------------------------
     //--------------------------------------------------------------------------------------
 
+    /**
+     * @return the name of the token.
+     */
+    function name() public pure returns (string memory) {
+        return "ether.fi ETH";
+    }
+
+    /**
+     * @return the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public pure returns (string memory) {
+        return "eETH";
+    }
+
+    /**
+     * @return the number of decimals for getting user representation of a token amount.
+     */
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
     function initialize(address _liquidityPool) external initializer {
-        __ERC20_init("EtherFi ETH", "eETH");
+        // __ERC20_init("EtherFi ETH", "eETH");
         __UUPSUpgradeable_init();
         __Ownable_init();
-        liquidityPool = _liquidityPool;
+        liquidityPool = ILiquidityPool(_liquidityPool);
     }
 
-    /// @notice function to mint eETH
-    /// @dev only able to mint from LiquidityPool contract
-    function mint(address _account, uint256 _amount) external onlyPoolContract {
-        _mint(_account, _amount);
+    function mintShares(address _user, uint256 _share) external onlyPoolContract {
+        shares[_user] += _share;
+        totalShares += _share;
     }
 
-    /// @notice function to burn eETH
-    /// @dev only able to burn from LiquidityPool contract
-    function burn(address _account, uint256 _amount) external onlyPoolContract {
-        _burn(_account, _amount);
+    function burnShares(address _user, uint256 _share) external onlyPoolContract {
+        shares[_user] -= _share;
+        totalShares -= _share;
+    }
+
+    // IERC20 functions
+
+    function totalSupply() public view returns (uint256) {
+        return liquidityPool.getTotalPooledEther();
+    }
+
+    function balanceOf(address _user) public view override(IEETH, IERC20Upgradeable) returns (uint256) {
+        return liquidityPool.getTotalEtherClaimOf(_user);
+    }
+
+    function transfer(address _recipient, uint256 _amount) external returns (bool) {
+        _transfer(msg.sender, _recipient, _amount);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) external view returns (uint256) {
+        return allowances[_owner][_spender];
+    }
+
+    function approve(address _spender, uint256 _amount) external returns (bool) {
+        _approve(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    function transferFrom(address _sender, address _recipient, uint256 _amount) external returns (bool) {
+        uint256 currentAllowance = allowances[_sender][msg.sender];
+        require(currentAllowance >= _amount, "TRANSFER_AMOUNT_EXCEEDS_ALLOWANCE");
+
+        _transfer(_sender, _recipient, _amount);
+        _approve(_sender, msg.sender, currentAllowance - _amount);
+        return true;
     }
 
     //--------------------------------------------------------------------------------------
     //-------------------------------  INTERNAL FUNCTIONS  ---------------------------------
     //--------------------------------------------------------------------------------------
+
+    function _transfer(address _sender, address _recipient, uint256 _amount) internal {
+        uint256 _sharesToTransfer = liquidityPool.sharesForAmount(_amount);
+        _transferShares(_sender, _recipient, _sharesToTransfer);
+        emit Transfer(_sender, _recipient, _amount);
+    }
+
+    function _approve(address _owner, address _spender, uint256 _amount) internal {
+        require(_owner != address(0), "APPROVE_FROM_ZERO_ADDRESS");
+        require(_spender != address(0), "APPROVE_TO_ZERO_ADDRESS");
+
+        allowances[_owner][_spender] = _amount;
+        emit Approval(_owner, _spender, _amount);
+    }
+
+    function _transferShares(address _sender, address _recipient, uint256 _sharesAmount) internal {
+        require(_sender != address(0), "TRANSFER_FROM_THE_ZERO_ADDRESS");
+        require(_recipient != address(0), "TRANSFER_TO_THE_ZERO_ADDRESS");
+        require(_sharesAmount <= shares[_sender], "TRANSFER_AMOUNT_EXCEEDS_BALANCE");
+
+        shares[_sender] -= _sharesAmount;
+        shares[_recipient] += _sharesAmount;
+    }
 
     function _authorizeUpgrade(
         address newImplementation
@@ -60,7 +141,7 @@ contract EETH is ERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable, IEETH {
     //--------------------------------------------------------------------------------------
 
     modifier onlyPoolContract() {
-        require(msg.sender == liquidityPool, "Only pool contract function");
+        require(msg.sender == address(liquidityPool), "Only pool contract function");
         _;
     }
 }
--- src/LiquidityPool.sol
+++ src/LiquidityPool.sol
@@ -6,13 +6,31 @@ import "@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol";
 import "./interfaces/IEETH.sol";
+import "./interfaces/IStakingManager.sol";
+import "./interfaces/IEtherFiNodesManager.sol";
+import "./interfaces/IScoreManager.sol";
+import "forge-std/console.sol";
 
 contract LiquidityPool is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     //--------------------------------------------------------------------------------------
     //---------------------------------  STATE-VARIABLES  ----------------------------------
     //--------------------------------------------------------------------------------------
 
-    address public eETH;
+    // TODO replace the below with the storage in ScoreManager
+    mapping(address => uint256) scores; // ScoreManager.scores
+    uint256 totalScores; // ScoreManager.totalScores
+
+    IEETH eETH; 
+    IStakingManager stakingManager; 
+    IEtherFiNodesManager nodesManager; 
+    IScoreManager scoreManager;
+
+    mapping(uint256 => bool) validators;
+    uint256 accruedSlashingPenalties;
+    uint256 accruedEapRewards;
+    uint256 bufferedEth;
+
+    uint64  numValidators;
 
     uint256[32] __gap;
 
@@ -34,18 +52,14 @@ contract LiquidityPool is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         __UUPSUpgradeable_init();
     }
 
-    /// @notice sets the contract address for eETH
-    /// @dev can't do it in constructor due to circular dependencies
-    /// @param _eETH address of eETH contract
-    function setTokenAddress(address _eETH) external {
-        eETH = _eETH;
-        emit TokenAddressChanged(_eETH);
-    }
-
     /// @notice deposit into pool
     /// @dev mints the amount of eTH 1:1 with ETH sent
     function deposit(address _user) external payable {
-        IEETH(eETH).mint(_user, msg.value);
+        uint256 share = _sharesForAmountAfterDeposit(msg.value);
+        if (share == 0) {
+            share = msg.value;
+        }
+        eETH.mintShares(_user, share);
 
         emit Deposit(_user, msg.value);
     }
@@ -53,26 +67,83 @@ contract LiquidityPool is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     /// @notice withdraw from pool
     /// @dev Burns user balance from msg.senders account & Sends equal amount of ETH back to user
     /// @param _amount the amount to withdraw from contract
+    /// TODO WARNING! This implementation does not take into consideration the score
     function withdraw(uint256 _amount) external payable {
-        require(
-            IERC20Upgradeable(eETH).balanceOf(msg.sender) >= _amount,
-            "Not enough eETH"
-        );
+        require(eETH.balanceOf(msg.sender) >= _amount, "Not enough eETH");
 
-        IEETH(eETH).burn(msg.sender, _amount);
+        uint256 share = sharesForAmount(_amount);
+        eETH.burnShares(msg.sender, share);
+
         (bool sent, ) = msg.sender.call{value: _amount}("");
         require(sent, "Failed to send Ether");
         emit Withdraw(msg.sender, msg.value);
     }
 
-    /// @notice Allows ether to be sent to this contract
-    receive() external payable {
-        emit Received(msg.sender, msg.value);
+    function getTotalEtherClaimOf(address _user) external view returns (uint256) {
+        uint256 staked;
+        uint256 boosted;
+        if (eETH.totalShares() > 0) {
+            staked = (getTotalPooledEther() * eETH.shares(_user)) / eETH.totalShares();
+        }
+        if (totalScores > 0) {
+            boosted = (accruedEapRewards * scores[_user]) / totalScores;
+        }
+        return staked + boosted;
+    }
+
+    function getEtherStakingPrincipal() public view returns (uint256) {
+        return (32 ether * numValidators) - accruedSlashingPenalties;
+    }
+
+    function getTotalPooledEther() public view returns (uint256) {
+        return getEtherStakingPrincipal() + address(this).balance - accruedEapRewards;
+    }
+
+    function sharesForAmount(uint256 _amount) public view returns (uint256) {
+        uint256 totalPooledEther = getTotalPooledEther();
+        if (totalPooledEther == 0) {
+            return 0;
+        }
+        return (_amount * eETH.totalShares()) / totalPooledEther;
+    }
+
+    function amountForShare(uint256 _share) public view returns (uint256) {
+        uint256 totalShares = eETH.totalShares();
+        if (totalShares == 0) {
+            return 0;
+        }
+        return (_share * getTotalPooledEther()) / eETH.totalShares();
+    }
+
+    /// @notice ether.fi protocol will send the ETH as the rewards for EAP users
+    function accrueEapRewards() external payable onlyOwner {
+        accruedEapRewards += msg.value;
+    }
+
+    /// @notice ether.fi protocol will be monitoring the status of validator nodes
+    ///         and update the accrued slashing penalties, if nay
+    function setAccruedSlashingPenalty(uint256 _amount) external onlyOwner {
+        accruedSlashingPenalties = _amount;
+    }
+
+    /// @notice sets the contract address for eETH
+    /// @param _eETH address of eETH contract
+    function setTokenAddress(address _eETH) external onlyOwner {
+        eETH = IEETH(_eETH);
+        emit TokenAddressChanged(_eETH);
     }
 
     //--------------------------------------------------------------------------------------
     //------------------------------  INTERNAL FUNCTIONS  ----------------------------------
     //--------------------------------------------------------------------------------------
+
+    function _sharesForAmountAfterDeposit(uint256 _amount) internal returns (uint256) {
+        uint256 totalPooledEther = getTotalPooledEther() - _amount;
+        if (totalPooledEther == 0) {
+            return 0;
+        }
+        return (_amount * eETH.totalShares()) / totalPooledEther;
+    }
 
     function _authorizeUpgrade(
         address newImplementation
