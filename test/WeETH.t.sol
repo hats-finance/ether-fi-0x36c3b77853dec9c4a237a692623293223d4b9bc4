4f909e22d670b83369a034e5db081bf0ebbd5af5
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-04-17 10:18:53 +0200

    stage

--- test/WeETH.t.sol
+++ test/WeETH.t.sol
@@ -66,6 +66,67 @@ contract WethETHTest is TestSetup {
     }
 
     function test_UnWrapWorksCorrectly() public {
-    
+        // Total pooled ether = 10
+        vm.deal(address(liquidityPoolInstance), 10 ether);
+        assertEq(liquidityPoolInstance.getTotalPooledEther(), 10 ether);
+        assertEq(eETHInstance.totalSupply(), 10 ether);
+
+        // Total pooled ether = 20
+        hoax(alice);
+        liquidityPoolInstance.deposit{value: 10 ether}(alice);
+
+        assertEq(liquidityPoolInstance.getTotalPooledEther(), 20 ether);
+        assertEq(eETHInstance.totalSupply(), 20 ether);
+
+        // ALice is first so get 100% of shares
+        assertEq(eETHInstance.shares(alice), 10 ether);
+        assertEq(eETHInstance.totalShares(), 10 ether);
+
+        // ALice total claimable Ether
+        /// (20 * 10) / 10
+        assertEq(liquidityPoolInstance.getTotalEtherClaimOf(alice), 20 ether);
+
+        hoax(bob);
+        liquidityPoolInstance.deposit{value: 5 ether}(bob);
+
+        assertEq(liquidityPoolInstance.getTotalPooledEther(), 25 ether);
+        assertEq(eETHInstance.totalSupply(), 25 ether);
+
+        // Bob Shares = (5 * 10) / (25 - 5) = 2,5
+        assertEq(eETHInstance.shares(bob), 2.5 ether);
+        assertEq(eETHInstance.totalShares(), 12.5 ether);
+
+        // Bob claimable Ether
+        /// (25 * 2,5) / 12,5 = 5 ether
+
+        //ALice Claimable Ether
+        /// (25 * 10) / 12,5 = 20 ether
+        assertEq(liquidityPoolInstance.getTotalEtherClaimOf(alice), 20 ether);
+        assertEq(liquidityPoolInstance.getTotalEtherClaimOf(bob), 5 ether);
+
+        assertEq(eETHInstance.balanceOf(alice), 20 ether);
+        assertEq(eETHInstance.balanceOf(bob), 5 ether);
+
+        // Staking Rewards sent to liquidity pool
+        /// vm.deal sets the balance of whoever its called on
+        /// In this case 10 ether is added as reward 
+        vm.deal(address(liquidityPoolInstance), 35 ether);
+        
+        assertEq(liquidityPoolInstance.getTotalPooledEther(), 35 ether);
+        assertEq(eETHInstance.totalSupply(), 35 ether);
+
+        // Bob claimable Ether
+        /// (35 * 2,5) / 12,5 = 7 ether
+        assertEq(liquidityPoolInstance.getTotalEtherClaimOf(bob), 7 ether);
+
+        // Alice Claimable Ether
+        /// (35 * 10) / 12,5 = 20 ether
+        assertEq(liquidityPoolInstance.getTotalEtherClaimOf(alice), 28 ether);
+
+        assertEq(eETHInstance.balanceOf(alice), 28 ether);
+        assertEq(eETHInstance.balanceOf(bob), 7 ether);
+
+        hoax(alice);
+        weEthInstance.wrap()
     }
 }
