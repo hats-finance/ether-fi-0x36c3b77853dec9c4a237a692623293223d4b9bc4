d6f80df828cd0ba3fcb21d3dddbfbad7c1368853
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-04-19 17:48:54 +0200

    Removed comments

--- src/RegulationsManager.sol
+++ src/RegulationsManager.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
-import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import "@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol";
 import "@openzeppelin-upgradeable/contracts/security/PausableUpgradeable.sol";
 import "@openzeppelin-upgradeable/contracts/security/ReentrancyGuardUpgradeable.sol";
