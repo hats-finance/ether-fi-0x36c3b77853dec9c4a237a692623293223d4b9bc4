0f5859672cc49153fe3f27a748c91dfc4dc3e113
Myles Traut <myles@linumlabs.com>
2023-04-25 22:21:59 +0200

    locked etherFi node implementation inititializer

--- src/EtherFiNode.sol
+++ src/EtherFiNode.sol
@@ -22,6 +22,11 @@ contract EtherFiNode is IEtherFiNode {
     //----------------------------------  CONSTRUCTOR   ------------------------------------
     //--------------------------------------------------------------------------------------
 
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        stakingStartTimestamp = uint32 (type(uint256).max);
+    }
+
     function initialize(address _etherFiNodesManager) public {
         require(stakingStartTimestamp == 0, "already initialised");
         require(_etherFiNodesManager != address(0), "No zero addresses");
