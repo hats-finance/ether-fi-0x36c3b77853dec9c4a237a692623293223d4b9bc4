32ac001f4c5b974ac40bee6722e5628f2f5a1b04
Myles Traut <myles@linumlabs.com>
2023-04-26 13:04:05 +0200

    removed unchecked function from auction manager

--- src/AuctionManager.sol
+++ src/AuctionManager.sol
@@ -134,7 +134,7 @@ contract AuctionManager is
         uint256[] memory bidIdArray = new uint256[](_bidSize);
         uint64[] memory ipfsIndexArray = new uint64[](_bidSize);
 
-        for (uint256 i = 0; i < _bidSize; i = uncheckedInc(i)) {
+        for (uint256 i = 0; i < _bidSize; i = ++i) {
             uint64 ipfsIndex = nodeOperatorManagerInterface.fetchNextKeyIndex(
                 msg.sender
             );
@@ -239,12 +239,6 @@ contract AuctionManager is
     //--------------------------------------------------------------------------------------
     //-------------------------------  INTERNAL FUNCTIONS   --------------------------------
     //--------------------------------------------------------------------------------------
-
-    function uncheckedInc(uint256 x) private pure returns (uint256) {
-        unchecked {
-            return x + 1;
-        }
-    }
 
     function _cancelBid(uint256 _bidId) internal {
 
