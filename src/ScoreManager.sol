2c13413ea3208de15ae90dd6de2a5192b3fd638a
Myles Traut <myles@linumlabs.com>
2023-05-08 11:33:32 +0200

    made suggested changes

--- src/ScoreManager.sol
+++ src/ScoreManager.sol
@@ -85,14 +85,14 @@ contract ScoreManager is
     /// @notice creates a new type of score
     /// @param _type the bytes value type being added
     function addNewScoreType(bytes memory _type) external onlyOwner returns (uint256) {
-        uint256 numberOfTypes_ = numberOfTypes;
-        scoreTypes[numberOfTypes_] = _type;
-        typeIds[_type] = numberOfTypes_;
+        uint256 nextTypeId = numberOfTypes;
+        scoreTypes[nextTypeId] = _type;
+        typeIds[_type] = nextTypeId;
 
-        emit NewTypeAdded(numberOfTypes_, _type);
+        emit NewTypeAdded(nextTypeId, _type);
 
         numberOfTypes++;
-        return numberOfTypes_;
+        return nextTypeId;
     }
 
     //--------------------------------------------------------------------------------------
