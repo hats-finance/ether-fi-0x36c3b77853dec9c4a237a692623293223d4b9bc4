d3866991535af62a0cb8f450383a64684398e38b
ReposCollector <seongyun@postech.ac.kr>
2023-06-06 08:11:21 +0900

    fixed the test in RewardsSkimmingTest

--- test/RewardsSkimmingTest.t.sol
+++ test/RewardsSkimmingTest.t.sol
@@ -51,6 +51,7 @@ contract RewardsSkimmingTest is TestSetup {
         vm.startPrank(owner);
         stakingManagerInstance.updateMerkleRoot(rootStakers);
         nodeOperatorManagerInstance.addToWhitelist(operators[0]);
+        stakingManagerInstance.setMaxBatchDepositSize(uint128(num_stakers));
         vm.stopPrank();
 
         startHoax(operators[0]);
@@ -69,11 +70,10 @@ contract RewardsSkimmingTest is TestSetup {
         }
         vm.stopPrank();
 
-        IStakingManager.DepositData[]
-            memory depositDataArray = new IStakingManager.DepositData[](num_stakers);
-
         for (uint i = 0; i < num_stakers; i++) {
             startHoax(stakers[i]);
+            IStakingManager.DepositData[] memory depositDataArray = new IStakingManager.DepositData[](1);
+
             uint256[] memory candidateBidIds = new uint256[](1);
             candidateBidIds[0] = validatorIds[i];
             bytes32[] memory stakerProof = merkleStakers.getProof(stakerWhitelistedAddresses, i);
@@ -87,16 +87,16 @@ contract RewardsSkimmingTest is TestSetup {
                 managerInstance.generateWithdrawalCredentials(etherFiNode),
                 32 ether
             );
-            depositDataArray[i] = IStakingManager.DepositData({
+            depositDataArray[0] = IStakingManager.DepositData({
                 publicKey: hex"8f9c0aab19ee7586d3d470f132842396af606947a0589382483308fdffdaf544078c3be24210677a9c471ce70b3b4c2c",
                 signature: hex"877bee8d83cac8bf46c89ce50215da0b5e370d282bb6c8599aabdbc780c33833687df5e1f5b5c2de8a6cd20b6572c8b0130b1744310a998e1079e3286ff03e18e4f94de8cdebecf3aaac3277b742adb8b0eea074e619c20d13a1dda6cba6e3df",
                 depositDataRoot: root,
                 ipfsHashForEncryptedValidatorKey: "test_ipfs"
             });
+
+            stakingManagerInstance.batchRegisterValidators(zeroRoot, candidateBidIds, depositDataArray);
             vm.stopPrank();
         }
-
-        stakingManagerInstance.batchRegisterValidators(zeroRoot, validatorIds, depositDataArray);
 
 
         // Mix the T-NFT holders
