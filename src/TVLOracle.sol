ed14ed6a429bf4bfad18e7f0626900b7663bec59
Dhruv Vora <dbvora03@gmail.com>
2023-06-21 08:52:39 -0500

    remove fallback

--- src/TVLOracle.sol
+++ src/TVLOracle.sol
@@ -15,8 +15,6 @@ contract TVLOracle is Ownable {
     tvlAggregator = _tvlAggregator;
   }
 
-  receive() external payable {}
-
   /// @notice Sets TVL by aggregator
   /// @param _newTvl New TVL calculated by aggregator
   function setTvl(uint256 _newTvl) public {
