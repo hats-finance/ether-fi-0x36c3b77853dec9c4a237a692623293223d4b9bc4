d6c55f7b0380da053a01601003c265e9dc697461
ReposCollector <seongyun@postech.ac.kr>
2023-06-27 13:46:40 +0100

    renamed to processDepositFromEapUser

--- src/interfaces/IMembershipNFT.sol
+++ src/interfaces/IMembershipNFT.sol
@@ -8,7 +8,7 @@ interface IMembershipNFT is IERC1155Upgradeable {
     function setMembershipManager(address _address) external;
     function convertEapPoints(uint256 _eapPoints, uint256 _ethAmount) external view returns (uint40, uint40);
     function setUpForEap(bytes32 _newMerkleRoot, uint64[] calldata _requiredEapPointsPerEapDeposit) external;
-    function processFreeMintForEapUserDeposit(address _user, uint256 _snapshotEthAmount, uint256 _points, bytes32[] calldata _merkleProof) external;
+    function processDepositFromEapUser(address _user, uint256 _snapshotEthAmount, uint256 _points, bytes32[] calldata _merkleProof) external;
     
     function incrementLock(uint256 _tokenId, uint256 blocks) external;
     function mint(address _to, uint256 _amount) external returns (uint256);
