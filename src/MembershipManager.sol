2be94bd058ab18c4b1d1deec539310946d32f718
ReposCollector <seongyun@postech.ac.kr>
2023-06-29 14:58:02 +0100

    one line

--- src/MembershipManager.sol
+++ src/MembershipManager.sol
@@ -428,15 +428,7 @@ contract MembershipManager is Initializable, OwnableUpgradeable, PausableUpgrade
         uint256 tokenId = membershipNFT.nextMintID();
         uint8 tier = tierForPoints(_tierPoints);
 
-        tokenData[tokenId] = TokenData(
-                                tierData[tier].rewardsGlobalIndex,
-                                _loyaltyPoints, 
-                                _tierPoints, 
-                                uint32(block.timestamp), 
-                                0, 
-                                tier, 
-                                0);
-
+        tokenData[tokenId] = TokenData(tierData[tier].rewardsGlobalIndex, _loyaltyPoints, _tierPoints, uint32(block.timestamp), 0, tier, 0);
 
         _deposit(tokenId, _amount, _amountForPoints);
 
