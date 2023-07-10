d20b1a2d4f0f902a98c1f1fd1739858215007d69
Seongyun <seongyun@postech.ac.kr>
2023-07-10 09:13:55 +0100

    Update DeployAndPopulateAddressProviderScript.s.sol

--- script/deploys/DeployAndPopulateAddressProviderScript.s.sol
+++ script/deploys/DeployAndPopulateAddressProviderScript.s.sol
@@ -39,7 +39,7 @@ contract DeployAndPopulateAddressProvider is Script {
         address owner = vm.envAddress("ADMIN");
         vm.startBroadcast(deployerPrivateKey);
 
-        addressProvider = new AddressProvider{salt: 0x6574686572666920697320617765736f6d650000000000000000000000000000}(owner);
+        addressProvider = new AddressProvider{salt: 0x727570657274206973206b696e67000000000000000000000000000000000000}(owner);
         console.log(address(addressProvider));
 
         /*---- Populate Registry ----*/
