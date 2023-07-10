adfdeacca12da15be9dcf0c6930321f4192c8ca0
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-07-10 11:35:49 +0200

    Updated updateAdminScript

--- script/specialized/UpdateAdminScripts.s.sol
+++ script/specialized/UpdateAdminScripts.s.sol
@@ -12,32 +12,30 @@ import "../../src/MembershipNFT.sol";
 import "../../src/StakingManager.sol";
 import "../../src/NFTExchange.sol";
 import "../../src/RegulationsManager.sol";
+import "../../src/helpers/AddressProvider.sol";
 
 contract UpdateAdmins is Script {   
+
+    AddressProvider public addressProvider;
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        address addressProviderAddress = vm.envAddress("CONTRACT_REGISTRY");
+        addressProvider = AddressProvider(addressProviderAddress);
+
         vm.startBroadcast(deployerPrivateKey);
 
-        address stakingManager = vm.envAddress("STAKING_MANAGER_PROXY_ADDRESS");
-        address etherFiNodesManager = vm.envAddress("ETHERFI_NODES_MANAGER_PROXY_ADDRESS");
-        address protocolRevenueManager = vm.envAddress("PROTOCOL_REVENUE_MANAGER_PROXY_ADDRESS");
-        address auctionManager = vm.envAddress("AUCTION_MANAGER_PROXY_ADDRESS");
-        address liquidityPool = vm.envAddress("LIQUIDITY_POOL_PROXY_ADDRESS");
-        address membershipManager = vm.envAddress("MEMBERSHIP_MANAGER_PROXY_ADDRESS");
-        address membershipNFT = vm.envAddress("MEMBERSHIP_NFT_PROXY_ADDRESS");
-        address nftExchange = vm.envAddress("NFT_EXCHANGE");
-        address regulationsManager = vm.envAddress("REGULATIONS_MANAGER_PROXY_ADDRESS");
+        address stakingManager = addressProvider.getProxyAddress("StakingManager");
+        address etherFiNodesManager = addressProvider.getProxyAddress("EtherFiNodesManager");
+        address protocolRevenueManager = addressProvider.getProxyAddress("ProtocolRevenueManager");
+        address auctionManager = addressProvider.getProxyAddress("AuctionManager");
         address admin = vm.envAddress("ADMIN");
 
         EtherFiNodesManager(payable(etherFiNodesManager)).updateAdmin(admin); 
         ProtocolRevenueManager(payable(protocolRevenueManager)).updateAdmin(admin); 
         AuctionManager(auctionManager).updateAdmin(admin); 
         StakingManager(stakingManager).updateAdmin(admin); 
-        LiquidityPool(payable(liquidityPool)).updateAdmin(admin); 
-        MembershipManager(payable(membershipManager)).updateAdmin(admin); 
-        MembershipNFT(membershipNFT).updateAdmin(admin); 
-        NFTExchange(nftExchange).updateAdmin(admin); 
-        RegulationsManager(regulationsManager).updateAdmin(admin); 
 
         vm.stopBroadcast();
     }
