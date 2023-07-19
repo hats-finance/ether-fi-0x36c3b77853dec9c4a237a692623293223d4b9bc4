82f9bd92de76e05097811f3264d23bc0a9103837
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2023-07-19 16:41:33 +0200

    g

--- script/deploys/DeployAndPopulateAddressProviderScript.s.sol
+++ script/deploys/DeployAndPopulateAddressProviderScript.s.sol
@@ -8,31 +8,32 @@ contract DeployAndPopulateAddressProvider is Script {
 
     /*---- Storage variables ----*/
 
-    struct ContractProxyAddresses {
-        address auctionManagerProxyAddress;
-        address stakingManagerProxyAddress;
-        address etherFiNodesManagerProxyAddress;
-        address protocolRevenueManagerProxy;
-        address tnftProxy;
-        address bnftProxy;
-    }
-
-    struct ContractImplementationAddresses {
-        address auctionManagerImplementationAddress;
-        address stakingManagerImplementationAddress;
-        address etherFiNodesManagerImplementationAddress;
+    struct PhaseOneAddresses {
+        address auctionManagerAddress;
+        address stakingManagerAddress;
+        address etherFiNodesManagerAddress;
+        address protocolRevenueManager;
+        address tnft;
+        address bnft;
         address treasury;
-        address protocolRevenueManagerImplementation;
-        address nodeOperatorManagerImplementation;
-        address tnftImplementation;
-        address bnftImplementation;
+        address nodeOperatorManager;
         address etherFiNode;
         address earlyAdopterPool;
     }
 
+    struct PhaseOnePointFiveAddress {
+        address eETH;
+        address liquidityPool;
+        address membershipManager;
+        address membershipNFT;
+        address nftExchange;
+        address regulationsManager;
+        address weETH;
+    }
+
     AddressProvider public addressProvider;
-    ContractProxyAddresses public contractProxyAddresses;
-    ContractImplementationAddresses public contractImplementationAddresses;
+    PhaseOneAddresses public phaseOneAddresses;
+    PhaseOnePointFiveAddress public phaseOnePointFiveAddress;
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
@@ -44,28 +45,41 @@ contract DeployAndPopulateAddressProvider is Script {
 
         /*---- Populate Registry ----*/
 
-        contractProxyAddresses.auctionManagerProxyAddress = vm.envAddress("AUCTION_MANAGER_PROXY_ADDRESS");
-        contractProxyAddresses.stakingManagerProxyAddress = vm.envAddress("STAKING_MANAGER_PROXY_ADDRESS");
-        contractProxyAddresses.etherFiNodesManagerProxyAddress = vm.envAddress("ETHERFI_NODES_MANAGER_PROXY_ADDRESS");
-        contractProxyAddresses.protocolRevenueManagerProxy = vm.envAddress("PROTOCOL_REVENUE_MANAGER_PROXY_ADDRESS");
-        contractProxyAddresses.tnftProxy = vm.envAddress("TNFT_PROXY_ADDRESS");
-        contractProxyAddresses.bnftProxy = vm.envAddress("BNFT_PROXY_ADDRESS");
+        phaseOneAddresses.auctionManagerAddress = vm.envAddress("AUCTION_MANAGER_PROXY_ADDRESS");
+        phaseOneAddresses.stakingManagerAddress = vm.envAddress("STAKING_MANAGER_PROXY_ADDRESS");
+        phaseOneAddresses.etherFiNodesManagerAddress = vm.envAddress("ETHERFI_NODES_MANAGER_PROXY_ADDRESS");
+        phaseOneAddresses.protocolRevenueManager = vm.envAddress("PROTOCOL_REVENUE_MANAGER_PROXY_ADDRESS");
+        phaseOneAddresses.tnft = vm.envAddress("TNFT_PROXY_ADDRESS");
+        phaseOneAddresses.bnft = vm.envAddress("BNFT_PROXY_ADDRESS");
+        phaseOneAddresses.treasury = vm.envAddress("TREASURY_ADDRESS");
+        phaseOneAddresses.nodeOperatorManager = vm.envAddress("NODE_OPERATOR_MANAGER_ADDRESS");
+        phaseOneAddresses.etherFiNode = vm.envAddress("ETHERFI_NODE");
+        phaseOneAddresses.earlyAdopterPool = vm.envAddress("EARLY_ADOPTER_POOL");
+        phaseOnePointFiveAddress.eETH = vm.envAddress("EETH_PROXY_ADDRESS");
+        phaseOnePointFiveAddress.liquidityPool = vm.envAddress("LIQUIDITY_POOL_PROXY_ADDRESS");
+        phaseOnePointFiveAddress.membershipManager = vm.envAddress("MEMBERSHIP_MANAGER_PROXY_ADDRESS");
+        phaseOnePointFiveAddress.membershipNFT = vm.envAddress("MEMBERSHIP_NFT_PROXY_ADDRESS");
+        phaseOnePointFiveAddress.nftExchange = vm.envAddress("NFT_EXCHANGE");
+        phaseOnePointFiveAddress.regulationsManager = vm.envAddress("REGULATIONS_MANAGER_PROXY_ADDRESS");
+        phaseOnePointFiveAddress.weETH = vm.envAddress("WEETH_PROXY_ADDRESS");
 
-        contractImplementationAddresses.treasury = vm.envAddress("TREASURY_ADDRESS");
-        contractImplementationAddresses.nodeOperatorManagerImplementation = vm.envAddress("NODE_OPERATOR_MANAGER_ADDRESS");
-        contractImplementationAddresses.etherFiNode = vm.envAddress("ETHERFI_NODE");
-        contractImplementationAddresses.earlyAdopterPool = vm.envAddress("EARLY_ADOPTER_POOL");
-
-        addressProvider.addContract(contractProxyAddresses.auctionManagerProxyAddress, "AuctionManager");
-        addressProvider.addContract(contractProxyAddresses.stakingManagerProxyAddress, "StakingManager");
-        addressProvider.addContract(contractProxyAddresses.etherFiNodesManagerProxyAddress, "EtherFiNodesManager");
-        addressProvider.addContract(contractProxyAddresses.protocolRevenueManagerProxy, "ProtocolRevenueManager");
-        addressProvider.addContract(contractProxyAddresses.tnftProxy, "TNFT");
-        addressProvider.addContract(contractProxyAddresses.bnftProxy, "BNFT");
-        addressProvider.addContract(contractImplementationAddresses.treasury, "Treasury");
-        addressProvider.addContract(contractImplementationAddresses.nodeOperatorManagerImplementation, "NodeOperatorManager");
-        addressProvider.addContract(contractImplementationAddresses.etherFiNode, "EtherFiNode");
-        addressProvider.addContract(contractImplementationAddresses.earlyAdopterPool, "EarlyAdopterPool");
+        addressProvider.addContract(phaseOneAddresses.auctionManagerAddress, "AuctionManager");
+        addressProvider.addContract(phaseOneAddresses.stakingManagerAddress, "StakingManager");
+        addressProvider.addContract(phaseOneAddresses.etherFiNodesManagerAddress, "EtherFiNodesManager");
+        addressProvider.addContract(phaseOneAddresses.protocolRevenueManager, "ProtocolRevenueManager");
+        addressProvider.addContract(phaseOneAddresses.tnft, "TNFT");
+        addressProvider.addContract(phaseOneAddresses.bnft, "BNFT");
+        addressProvider.addContract(phaseOneAddresses.treasury, "Treasury");
+        addressProvider.addContract(phaseOneAddresses.nodeOperatorManager, "NodeOperatorManager");
+        addressProvider.addContract(phaseOneAddresses.etherFiNode, "EtherFiNode");
+        addressProvider.addContract(phaseOneAddresses.earlyAdopterPool, "EarlyAdopterPool");
+        addressProvider.addContract(phaseOnePointFiveAddress.eETH, "EETH");
+        addressProvider.addContract(phaseOnePointFiveAddress.liquidityPool, "LiquidityPool");
+        addressProvider.addContract(phaseOnePointFiveAddress.membershipManager, "MembershipManager");
+        addressProvider.addContract(phaseOnePointFiveAddress.membershipNFT, "MembershipNFT");
+        addressProvider.addContract(phaseOnePointFiveAddress.nftExchange, "NFTExchange");
+        addressProvider.addContract(phaseOnePointFiveAddress.regulationsManager, "RegulationsManager");
+        addressProvider.addContract(phaseOnePointFiveAddress.weETH, "WeETH");
 
         vm.stopBroadcast();
     }
