df21ea892f407072246e38d91e01ccffad8925bc
dave <dave.alexander@hey.com>
2023-08-17 14:29:19 -0600

    track exchange rate and emit alongside events

--- src/LPAPoints.sol
+++ src/LPAPoints.sol
@@ -5,10 +5,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LPAPoints is Ownable {
 
-    event PointsPurchased(address indexed buyer, uint256 indexed tokenId, uint256 amountWei);
+    event PointsPurchased(address indexed buyer, uint256 indexed tokenId, uint256 amountWei, uint256 weiPerPoint);
+
+    uint256 public weiPerPoint;
+
+    constructor(uint256 _weiPerPoint) {
+        weiPerPoint = _weiPerPoint;
+    }
 
     function purchasePoints(uint256 tokenId) external payable {
-        emit PointsPurchased(msg.sender, tokenId, msg.value);
+        emit PointsPurchased(msg.sender, tokenId, msg.value, weiPerPoint);
     }
 
     //-----------------------------------------------------------------------------
@@ -17,5 +23,9 @@ contract LPAPoints is Ownable {
 
     function withdrawFunds(address payable _to) external onlyOwner {
         _to.transfer(address(this).balance);
+    }
+
+    function setWeiPerPoint(uint256 _weiPerPoint) external onlyOwner {
+        weiPerPoint = _weiPerPoint;
     }
 }
