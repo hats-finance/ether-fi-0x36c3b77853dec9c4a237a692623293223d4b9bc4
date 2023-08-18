29a68db1f48bf9c06f099c386d0c169f1d3ee56c
Rupert Klopper <rupert@ether.fi>
2023-08-17 22:37:11 -0500

    add deploy script for LPA points contract

---
+++ script/deploys/DeployLPAPoints.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../../src/UUPSProxy.sol";
+import "../../src/LPAPoints.sol";
+import "../../src/helpers/AddressProvider.sol";
+
+contract DeployLPAPointsScript is Script {
+
+    LPAPoints public lpaPoints;
+    AddressProvider public addressProvider;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        
+        address addressProviderAddress = vm.envAddress("CONTRACT_REGISTRY");
+
+        addressProvider = AddressProvider(addressProviderAddress);
+        
+        vm.startBroadcast(deployerPrivateKey);
+
+        lpaPoints = new LPAPoints(1000000000000);
+
+        addressProvider.addContract(address(lpaPoints), "LPAPoints");
+
+        vm.stopBroadcast();
+    }
+}
