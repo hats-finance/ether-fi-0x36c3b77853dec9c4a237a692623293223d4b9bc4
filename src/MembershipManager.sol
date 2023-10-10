b71b81e87c0a375a91c80d081072c81a40e9efe0
ReposCollector <seongyun@postech.ac.kr>
2023-10-10 12:12:29 +0900

    SD-14 MembershipManager.addNewTier uses the wrong length

--- src/MembershipManager.sol
+++ src/MembershipManager.sol
@@ -287,12 +287,11 @@ contract MembershipManager is Initializable, OwnableUpgradeable, PausableUpgrade
     }
 
     error TierLimitExceeded();
-    function addNewTier(uint40 _requiredTierPoints, uint24 _weight) external returns (uint256) {
+    function addNewTier(uint40 _requiredTierPoints, uint24 _weight) external {
         _requireAdmin();
         if (tierDeposits.length >= type(uint8).max) revert TierLimitExceeded();
         tierData.push(TierData(0, _requiredTierPoints, _weight, 0));
         tierVaults.push(TierVault(0, 0));
-        return tierDeposits.length - 1;
     }
 
     error OutOfBound();
