5398101f9457df411c8dedb908af7629dc3a1255
ReposCollector <seongyun@postech.ac.kr>
2023-11-01 14:49:18 -0500

    fix

--- test/TestSetup.sol
+++ test/TestSetup.sol
@@ -713,7 +713,7 @@ contract TestSetup is Test {
         vm.prank(bob);
         etherFiOracleInstance.submitReport(_report);
 
-        moveClock(int256(int16(etherFiAdminInstance.postReportWaitTimeInSlots())));
+        _moveClock(int256(int16(etherFiAdminInstance.postReportWaitTimeInSlots())));
 
         if (bytes(_revertMessage).length > 0) {
             vm.expectRevert(bytes(_revertMessage));
