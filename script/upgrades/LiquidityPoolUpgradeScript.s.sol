9cba773798e93327a29612618c6adc53aab9ee55
ReposCollector <seongyun@postech.ac.kr>
2023-11-01 22:32:33 -0500

    Updated LiquidityPool upgrade script

--- script/upgrades/LiquidityPoolUpgradeScript.s.sol
+++ script/upgrades/LiquidityPoolUpgradeScript.s.sol
@@ -9,6 +9,13 @@ contract LiquidityPoolUpgrade is Script {
   
     AddressProvider public addressProvider;
 
+    address public stakingManager;
+    uint256 public getTotalPooledEther;
+    uint32 public numPendingDeposits;
+    uint128 public totalValueOutOfLp;
+    uint128 public totalValueInLp;
+
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
@@ -22,18 +29,38 @@ contract LiquidityPoolUpgrade is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         LiquidityPool LiquidityPoolInstance = LiquidityPool(payable(LiquidityPoolProxyAddress));
+
+        // Copy the states
+        stakingManager = address(LiquidityPoolInstance.stakingManager());
+        getTotalPooledEther = LiquidityPoolInstance.getTotalPooledEther();
+        numPendingDeposits = LiquidityPoolInstance.numPendingDeposits();
+        totalValueOutOfLp = LiquidityPoolInstance.totalValueOutOfLp();
+        totalValueInLp = LiquidityPoolInstance.totalValueInLp();
+
         LiquidityPool LiquidityPoolV2Implementation = new LiquidityPool();
 
         LiquidityPoolInstance.upgradeTo(address(LiquidityPoolV2Implementation));
 
         // Phase 2
-        // TODO: Set the correct values for the below parameters
         //Ensure these inputs are correct
         //First parameter = the scheduling period in seconds we want to set
         //Second parameter = the number of validators ETH source of funds currently has spun up
         //Third parameter = the number of validators ETHER_FAN source of funds currently has spun up
-        LiquidityPoolInstance.initializeOnUpgrade(900, 3, 9, etherFiAdminAddress, withdrawRequestNFTAddress);
-        LiquidityPoolInstance.setNumValidatorsToSpinUpPerSchedulePerBnftHolder(4);
+        if (block.chainid == 1) {
+            // TODO - set the correct value for the 3rd parameter
+            LiquidityPoolInstance.initializeOnUpgrade(1 days, 0, 260, etherFiAdminAddress, withdrawRequestNFTAddress);
+        } else if (block.chainid == 5) {
+            LiquidityPoolInstance.initializeOnUpgrade(900, 3, 9, etherFiAdminAddress, withdrawRequestNFTAddress);
+        } else {
+            require(false, "chain is wrong");
+        }
+
+        // Validate the states
+        require(stakingManager == address(LiquidityPoolInstance.stakingManager()), "stakingManager");
+        require(getTotalPooledEther == LiquidityPoolInstance.getTotalPooledEther(), "getTotalPooledEther");
+        require(numPendingDeposits == LiquidityPoolInstance.numPendingDeposits(), "numPendingDeposits");
+        require(totalValueOutOfLp == LiquidityPoolInstance.totalValueOutOfLp(), "totalValueOutOfLp");
+        require(totalValueInLp == LiquidityPoolInstance.totalValueInLp(), "totalValueInLp");
 
         vm.stopBroadcast();
     }
